97357faeb547570651061258427aee427dda02a0
Coleman Watts <coleman@civicrm.org>
2017-01-23 20:19:26 -0500

    Add activity feed control bar

--- ang/civicase/ActivityFeed.js
+++ ang/civicase/ActivityFeed.js
@@ -7,8 +7,10 @@
         resolve: {
           data: function(crmApi) {
             return crmApi({
-              statuses: ['optionValue', 'get', {options: {limit: 0, sort: 'weight'}, 'option_group_id': 'activity_status'}],
-              types: ['optionValue', 'get', {options: {limit: 0, sort: 'weight'}, 'option_group_id': 'activity_type'}]
+              tags: ['tag', 'get', {options: {limit: 0}, used_for: {LIKE: '%civicrm_activity%'}, return: ['color', 'name', 'description']}],
+              statuses: ['optionValue', 'get', {options: {limit: 0, sort: 'weight'}, 'option_group_id': 'activity_status', is_active: 1}],
+              types: ['optionValue', 'get', {options: {limit: 0, sort: 'weight'}, 'option_group_id': 'activity_type', is_active: 1}],
+              categories: ['optionValue', 'get', {options: {limit: 0, sort: 'weight'}, 'option_group_id': 'activity_category', is_active: 1}]
             });
           }
         }
@@ -33,17 +35,17 @@
     }
 
     // We have data available in JS. We also want to reference in HTML.
-    activityTypes = $scope.activityTypes = _.indexBy(data.types.values, 'value');
-    activityStatuses = $scope.activityStatuses = _.indexBy(data.statuses.values, 'value');
+    var activityTypes = $scope.activityTypes = _.indexBy(data.types.values, 'value');
+    var activityStatuses = $scope.activityStatuses = _.indexBy(data.statuses.values, 'value');
+    var activityCategories = $scope.activityCategories = _.indexBy(data.categories.values, 'value');
+    var tags = $scope.tags = data.tags.values;
     $scope.activityTypeOptions = _.map(data.types.values, mapSelectOptions);
     $scope.activityStatusOptions = _.map(data.statuses.values, mapSelectOptions);
     $scope.activities = {};
-
     $scope.availableFilters = {
       activity_type_id: ts('Activity type'),
       status_id: ts('Status'),
-      subject: ts('Subject'),
-      details: ts('Details'),
+      text: ts('Contains text'),
       target_contact_id: ts('With'),
       assignee_contact_id: ts('Assigned to'),
       tag_id: ts('Tagged')
@@ -56,8 +58,19 @@
 
     $scope.exposedFilters = CRM.cache.get('activityFeedFilters', {
       activity_type_id: true,
-      status_id: true
+      status_id: true,
+      text: true
     });
+
+    var displayOptions = $scope.displayOptions = CRM.cache.get('activityFeedDisplayOptions', {
+      followup_nested: true,
+      overdue_first: true
+    });
+
+    var involving = $scope.involving = {
+      myActivities: false,
+      delegated: false
+    };
 
     $scope.star = function(act) {
       act.is_star = act.is_star === '1' ? '0' : '1';
@@ -100,27 +113,39 @@
     function _loadActivities() {
       var params = {
         sequential: 1,
-        source_contact_id: 'user_contact_id',
-        options: {sort: 'activity_date_time DESC'},
         is_current_revision: 1,
         is_deleted: 0,
-        return: ['subject', 'details', 'activity_type_id', 'status_id', 'source_contact_name', 'target_contact_name', 'assignee_contact_name', 'activity_date_time', 'is_star'],
-        "api.EntityTag.get": {entity_table: 'civicrm_activity', return: ['tag_id.name', 'tag_id.color', 'tag_id.description']},
-        "api.Attachment.get": {entity_table: 'civicrm_activity'}
+        return: ['subject', 'details', 'activity_type_id', 'status_id', 'source_contact_name', 'target_contact_name', 'assignee_contact_name', 'activity_date_time', 'is_star', 'original_id', 'tag_id'],
+        "api.Attachment.get": {entity_table: 'civicrm_activity'},
+        options: {
+          sort: 'activity_date_time DESC'
+        }
       };
       _.each($scope.filters, function(val, key) {
         if (val) {
-          if (key === 'subject' || key === 'details') {
+          if (key === 'text') {
+            params.subject = {LIKE: '%' + val + '%'};
+            params.details = {LIKE: '%' + val + '%'};
+            params.options.or = [['subject', 'details']];
+          }
+          else if (key === 'activity_type_id.grouping') {
             params[key] = {LIKE: '%' + val + '%'};
           } else {
             params[key] = {IN: val.split(',')};
           }
         }
       });
-      return crmApi('Activity', 'get', params);
+      if (involving.myActivities) {
+        params.contact_id = 'user_contact_id';
+      }
+      if (involving.delegated && !params.assignee_contact_id) {
+        params.assignee_contact_id = {'!=': 'user_contact_id'};
+      }
+      return crmApi('Activity', 'get', $.extend(true, returnParams, params));
     }
 
     $scope.$watchCollection('filters', getActivities);
+    $scope.$watchCollection('involving', getActivities);
 
     $scope.$watchCollection('exposedFilters', function() {
       CRM.cache.set('activityFeedFilters', $scope.exposedFilters);
@@ -129,6 +154,11 @@
           delete $scope.filters[key];
         }
       });
+    });
+
+    $scope.$watchCollection('displayOptions', function() {
+      CRM.cache.set('activityFeedDisplayOptions', displayOptions);
+      getActivities();
     });
 
     // Respond to activities edited in popups.
