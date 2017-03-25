441ca694c5b60e7eb226eeeb106234d6ece4df1a
Tim Otten <totten@civicrm.org>
2017-03-24 17:49:40 -0700

    Implement Case.getfiles API

---
+++ api/v3/Case/Getfiles.php
@@ -0,0 +1,144 @@
+<?php
+
+/**
+ * Case.Getfiles API specification (optional)
+ * This is used for documentation and validation.
+ *
+ * @param array $spec description of fields supported by this API call
+ * @return void
+ * @see http://wiki.civicrm.org/confluence/display/CRMDOC/API+Architecture+Standards
+ */
+function _civicrm_api3_case_getfiles_spec(&$spec) {
+  $spec['case_id']['api.required'] = 1;
+}
+
+/**
+ * Case.Getfiles API
+ *
+ * Perform a search for files related to a case.
+ *
+ * @param array $params
+ * @return array API result descriptor
+ * @see civicrm_api3_create_success
+ * @see civicrm_api3_create_error
+ * @throws API_Exception
+ */
+function civicrm_api3_case_getfiles($params) {
+  // Check authorization.
+  // WISHLIST: would be nice to incorporate addSelectWhere() instead?
+  _civicrm_api3_case_getfiles_assert_access($params);
+  $options = _civicrm_api3_get_options_from_params($params);
+
+  $matches = _civicrm_api3_case_getfiles_find($params, $options);
+  $result = civicrm_api3_create_success($matches);
+  if (!empty($params['options']['xref'])) {
+    $result['xref'] = _civicrm_api3_case_getfiles_xref($matches);
+  }
+  return $result;
+}
+
+/**
+ * @param $params
+ * @param $options
+ * @return array
+ *   Ex: array(0 => array('case_id' => 123, 'activity_id' => 456, 'file_id' => 789))
+ */
+function _civicrm_api3_case_getfiles_find($params, $options) {
+  $select = _civicrm_api3_case_getfiles_select($params);
+
+  if (!empty($options['limit'])) {
+    $select->limit($options['limit'], isset($options['offset']) ? $options['offset'] : 0);
+  }
+
+  $dao = \CRM_Core_DAO::executeQuery($select->toSQL());
+  $matches = array();
+  while ($dao->fetch()) {
+    $matches[$dao->id] = $dao->toArray();
+  }
+  if (!empty($params['sequential'])) {
+    $matches = array_values($matches);
+  }
+  return $matches;
+}
+
+/**
+ * @param array $params
+ * @return CRM_Utils_SQL_Select
+ */
+function _civicrm_api3_case_getfiles_select($params) {
+  $select = CRM_Utils_SQL_Select::from('civicrm_case_activity caseact')
+    ->join('act', 'INNER JOIN civicrm_activity act ON ((caseact.id = act.id OR caseact.activity_id = act.original_id) AND act.is_current_revision=1)')
+    ->join('ef', 'INNER JOIN civicrm_entity_file ef ON (ef.entity_table = "civicrm_activity" AND ef.entity_id = caseact.activity_id) ')
+    ->join('f', 'INNER JOIN civicrm_file f ON ef.file_id = f.id')
+    ->select('caseact.case_id as case_id, act.id as activity_id, f.id as id')
+    ->distinct();
+
+  if (isset($params['case_id'])) {
+    // Isn't there some helper which will let us do more advanced SQL with $params['case_id']?
+    $select->where('caseact.case_id = #caseIDs', array(
+      'caseIDs' => $params['case_id'],
+    ));
+  }
+
+  if (isset($params['text'])) {
+    $select->where('act.subject LIKE @q OR act.details LIKE @q OR f.description LIKE @q OR f.uri LIKE @q',
+      array('q' => '%' . $params['text'] . '%'));
+  }
+
+  $select->orderBy(array('caseact.activity_id DESC, f.id ASC'));
+  return $select;
+}
+
+/**
+ * Assert that this request is authorized to access the given Case.
+ * @param array $params
+ * @throws API_Exception
+ */
+function _civicrm_api3_case_getfiles_assert_access($params) {
+  if (empty($params['check_permissions'])) {
+    return; // OK
+  }
+
+  if (empty($params['case_id'])) {
+    throw new API_Exception("Blank case_id cannot be validated");
+  }
+
+  // Delegate to Case.get to determine if the ID is accessible.
+  civicrm_api3('Case', 'getsingle', array(
+    'id' => $params['case_id'],
+    'return' => 'id',
+  ));
+}
+
+/**
+ * Lookup any cross-references in the `getfiles` data.
+ *
+ * @param array $matches
+ *   Ex: array(0 => array('case_id' => 123, 'activity_id' => 456, 'id' => 789))
+ * @return array
+ *   Ex:
+ *     $result['case'][123]['case_type_id'] = 3;
+ *     $result['activity'][456]['subject'] = 'the subject';
+ *     $result['file'][789]['mime_type'] = 'text/plain';
+ */
+function _civicrm_api3_case_getfiles_xref($matches) {
+  $types = array(
+    // array(string $idField, string $xrefName, string $apiEntity)
+    array('case_id', 'case', 'Case'),
+    array('activity_id', 'activity', 'Activity'),
+    array('id', 'file', 'Attachment'),
+  );
+
+  $result = array();
+  foreach ($types as $typeSpec) {
+    list ($idField, $xrefName, $apiEntity) = $typeSpec;
+    $ids = array_unique(CRM_Utils_Array::collect($idField, $matches));
+    // WISH: $result[$xrefName] = civicrm_api3($apiEntity, 'get', array('id'=>array('IN', $ids)))['values'];
+    foreach ($ids as $id) {
+      $result[$xrefName][$id] = civicrm_api3($apiEntity, 'getsingle', array(
+        'id' => $id,
+      ));
+    }
+  }
+  return $result;
+}
---
+++ tests/phpunit/api/v3/Case/GetfilesTest.php
@@ -0,0 +1,337 @@
+<?php
+
+use Civi\Test\HeadlessInterface;
+use Civi\Test\HookInterface;
+use Civi\Test\TransactionalInterface;
+
+require_once 'BaseTestCase.php';
+
+/**
+ * Test the "Case.getfiles" API.
+ *
+ * Tips:
+ *  - With HookInterface, you may implement CiviCRM hooks directly in the test class.
+ *    Simply create corresponding functions (e.g. "hook_civicrm_post(...)" or similar).
+ *  - With TransactionalInterface, any data changes made by setUp() or test****() functions will
+ *    rollback automatically -- as long as you don't manipulate schema or truncate tables.
+ *    If this test needs to manipulate schema or truncate tables, then either:
+ *       a. Do all that using setupHeadless() and Civi\Test.
+ *       b. Disable TransactionalInterface, and handle all setup/teardown yourself.
+ *
+ * @group headless
+ */
+class api_v3_Case_GetfilesTest extends api_v3_Case_BaseTestTest implements HeadlessInterface, HookInterface, TransactionalInterface {
+
+  public function setUpHeadless() {
+    // Civi\Test has many helpers, like install(), uninstall(), sql(), and sqlFile().
+    // See: https://github.com/civicrm/org.civicrm.testapalooza/blob/master/civi-test.md
+    return \Civi\Test::headless()->installMe(__DIR__)->apply();
+  }
+
+  public function setUp() {
+    parent::setUp();
+    $this->cleanupFiles();
+  }
+
+  public function tearDown() {
+    parent::tearDown();
+    $this->cleanupFiles();
+  }
+
+  public function getExamples() {
+    $cases = array();
+
+    // $cases[] = array(
+    //   0 => 'actSubject',
+    //   1 => 'actDetails',
+    //   2 => 'fileName',
+    //   3 => 'searchText',
+    //   4 => expectMatch,
+    // );
+
+    $cases[] = array(
+      // Match any file if there's no textual filter.
+      0 => 'Give bread a chance',
+      1 => 'With a little butter and jam',
+      2 => self::getFilePrefix() . 'theStuff.txt',
+      3 => '',
+      4 => TRUE,
+    );
+    $cases[] = array(
+      // This doesn't match "cheese" to anything.
+      0 => 'Give bread a chance',
+      1 => 'With a little butter and jam',
+      2 => self::getFilePrefix() . 'theStuff.txt',
+      3 => 'cheese',
+      4 => FALSE,
+    );
+    $cases[] = array(
+      // Match on subject.
+      0 => 'Give cheese a chance',
+      1 => '',
+      2 => self::getFilePrefix() . 'theStuff.txt',
+      3 => 'cheese',
+      4 => TRUE,
+    );
+    $cases[] = array(
+      // Match on details.
+      0 => 'Give bread a chance',
+      1 => 'But make it with cheesey goodness',
+      2 => self::getFilePrefix() . 'theStuff.txt',
+      3 => 'cheese',
+      4 => TRUE,
+    );
+    $cases[] = array(
+      // Match on file name.
+      0 => 'Give bread a chance',
+      1 => '',
+      2 => self::getFilePrefix() . 'theCheeseIsGoodForYou.txt',
+      3 => 'cheese',
+      4 => TRUE,
+    );
+    return $cases;
+  }
+
+  /**
+   * Create an activity with an attachment. Run a search. See if it matches.
+   *
+   * @param string $actSubject
+   *   Set the activity's subject
+   * @param string $actDetails
+   *   Set the activity's details.
+   * @param string $fileName
+   *   Set the attachment's file name.
+   * @param string $searchText
+   *   Search for the given text.
+   * @param bool $expectMatch
+   *   Whether the $searchText matches the activity.
+   * @dataProvider getExamples
+   */
+  public function testSearch($actSubject, $actDetails, $fileName, $searchText, $expectMatch) {
+    $cases[0] = $this->callAPISuccess('Case', 'create', array(
+      'contact_id' => 1,
+      'creator_id' => 1,
+      'case_type_id' => 'housing_support',
+      'subject' => 'Hello world',
+    ));
+    $cases[1] = $this->callAPISuccess('Case', 'create', array(
+      'contact_id' => 2,
+      'creator_id' => 2,
+      'case_type_id' => 'housing_support',
+      'subject' => 'Hello world',
+    ));
+
+    $medEval = $this->callAPISuccess('Activity', 'getsingle', array(
+      'case_id' => $cases[0]['id'],
+      'activity_type_id' => 'Medical evaluation',
+    ));
+    CRM_Core_DAO::executeQuery('UPDATE civicrm_activity SET subject = %2, details = %3 WHERE id = %1', array(
+      1 => array($medEval['id'], 'Integer'),
+      2 => array($actSubject, 'String'),
+      3 => array($actDetails, 'String'),
+    ));
+    $attachment = $this->callAPISuccess('Attachment', 'create', array(
+      'name' => $fileName,
+      'mime_type' => 'text/plain',
+      'content' => 'My test content',
+      'entity_table' => 'civicrm_activity',
+      'entity_id' => $medEval['id'],
+    ));
+
+    $getfiles = $this->callAPISuccess('Case', 'getfiles', array(
+      'case_id' => $cases[0]['id'],
+      'text' => $searchText,
+    ));
+    if ($expectMatch) {
+      $attId = $attachment['id'];
+      $this->assertEquals(1, $getfiles['count']);
+      $this->assertEquals($cases[0]['id'], $getfiles['values'][$attId]['case_id']);
+      $this->assertEquals($medEval['id'], $getfiles['values'][$attId]['activity_id']);
+      $this->assertEquals($attachment['id'], $getfiles['values'][$attId]['id']);
+      $this->assertTrue(!isset($getfiles['xref']));
+    }
+    else {
+      $this->assertEquals(0, $getfiles['count']);
+      $this->assertTrue(!isset($getfiles['xref']));
+    }
+  }
+
+  /**
+   * Create an activity with an attachment. Run a search. See if it matches.
+   *
+   * @param string $actSubject
+   *   Set the activity's subject
+   * @param string $actDetails
+   *   Set the activity's details.
+   * @param string $fileName
+   *   Set the attachment's file name.
+   * @param string $searchText
+   *   Search for the given text.
+   * @param bool $expectMatch
+   *   Whether the $searchText matches the activity.
+   * @dataProvider getExamples
+   */
+  public function testReviseThenAttachThenSearch($actSubject, $actDetails, $fileName, $searchText, $expectMatch) {
+    $cases[0] = $this->callAPISuccess('Case', 'create', array(
+      'contact_id' => 1,
+      'creator_id' => 1,
+      'case_type_id' => 'housing_support',
+      'subject' => 'Hello world',
+    ));
+    $cases[1] = $this->callAPISuccess('Case', 'create', array(
+      'contact_id' => 2,
+      'creator_id' => 2,
+      'case_type_id' => 'housing_support',
+      'subject' => 'Hello world',
+    ));
+
+    $medEval = $this->callAPISuccess('Activity', 'getsingle', array(
+      'case_id' => $cases[0]['id'],
+      'activity_type_id' => 'Medical evaluation',
+    ));
+    $update = $this->callAPISuccess('Activity', 'create', array(
+      'activity_id' => $medEval['id'],
+      'subject' => $actSubject,
+      'details' => $actDetails,
+    ));
+    $attachment = $this->callAPISuccess('Attachment', 'create', array(
+      'name' => $fileName,
+      'mime_type' => 'text/plain',
+      'content' => 'My test content',
+      'entity_table' => 'civicrm_activity',
+      'entity_id' => $update['id'],
+    ));
+
+    $getfiles = $this->callAPISuccess('Case', 'getfiles', array(
+      'case_id' => $cases[0]['id'],
+      'text' => $searchText,
+    ));
+    if ($expectMatch) {
+      $attId = $attachment['id'];
+      $this->assertEquals(1, $getfiles['count']);
+      $this->assertEquals($cases[0]['id'], $getfiles['values'][$attId]['case_id']);
+      $this->assertEquals($update['id'], $getfiles['values'][$attId]['activity_id']);
+      $this->assertEquals($attachment['id'], $getfiles['values'][$attId]['id']);
+      $this->assertTrue(!isset($getfiles['xref']));
+    }
+    else {
+      $this->assertEquals(0, $getfiles['count']);
+      $this->assertTrue(!isset($getfiles['xref']));
+    }
+  }
+
+  /**
+   * Create an activity with an attachment. Run a search. See if it matches.
+   *
+   * @param string $actSubject
+   *   Set the activity's subject
+   * @param string $actDetails
+   *   Set the activity's details.
+   * @param string $fileName
+   *   Set the attachment's file name.
+   * @param string $searchText
+   *   Search for the given text.
+   * @param bool $expectMatch
+   *   Whether the $searchText matches the activity.
+   * @dataProvider getExamples
+   */
+  public function testAttachThenReviseThenSearch($actSubject, $actDetails, $fileName, $searchText, $expectMatch) {
+    $cases[0] = $this->callAPISuccess('Case', 'create', array(
+      'contact_id' => 1,
+      'creator_id' => 1,
+      'case_type_id' => 'housing_support',
+      'subject' => 'Hello world',
+    ));
+    $cases[1] = $this->callAPISuccess('Case', 'create', array(
+      'contact_id' => 2,
+      'creator_id' => 2,
+      'case_type_id' => 'housing_support',
+      'subject' => 'Hello world',
+    ));
+
+    $medEval = $this->callAPISuccess('Activity', 'getsingle', array(
+      'case_id' => $cases[0]['id'],
+      'activity_type_id' => 'Medical evaluation',
+    ));
+    $attachment = $this->callAPISuccess('Attachment', 'create', array(
+      'name' => $fileName,
+      'mime_type' => 'text/plain',
+      'content' => 'My test content',
+      'entity_table' => 'civicrm_activity',
+      'entity_id' => $medEval['id'],
+    ));
+    $update = $this->callAPISuccess('Activity', 'create', array(
+      'activity_id' => $medEval['id'],
+      'subject' => $actSubject,
+      'details' => $actDetails,
+    ));
+
+    $getfiles = $this->callAPISuccess('Case', 'getfiles', array(
+      'case_id' => $cases[0]['id'],
+      'text' => $searchText,
+    ));
+    if ($expectMatch) {
+      $attId = $attachment['id'];
+      $this->assertEquals(1, $getfiles['count']);
+      $this->assertEquals($cases[0]['id'], $getfiles['values'][$attId]['case_id']);
+      $this->assertEquals($update['id'], $getfiles['values'][$attId]['activity_id']);
+      $this->assertEquals($attachment['id'], $getfiles['values'][$attId]['id']);
+      $this->assertTrue(!isset($getfiles['xref']));
+    }
+    else {
+      $this->assertEquals(0, $getfiles['count']);
+      $this->assertTrue(!isset($getfiles['xref']));
+    }
+  }
+
+  /**
+   * Test that `options.xref==1` causes lookups for all related records.
+   */
+  public function testXref() {
+    $cases[0] = $this->callAPISuccess('Case', 'create', array(
+      'contact_id' => 1,
+      'creator_id' => 1,
+      'case_type_id' => 'housing_support',
+      'subject' => 'Hello world',
+    ));
+
+    $medEval = $this->callAPISuccess('Activity', 'getsingle', array(
+      'case_id' => $cases[0]['id'],
+      'activity_type_id' => 'Medical evaluation',
+    ));
+    CRM_Core_DAO::executeQuery('UPDATE civicrm_activity SET subject = %2, details = %3 WHERE id = %1', array(
+      1 => array($medEval['id'], 'Integer'),
+      2 => array('The subject', 'String'),
+      3 => array('The details', 'String'),
+    ));
+    $attachment = $this->callAPISuccess('Attachment', 'create', array(
+      'name' => self::getFilePrefix() . 'TheFile.txt',
+      'mime_type' => 'text/plain',
+      'content' => 'My test content',
+      'entity_table' => 'civicrm_activity',
+      'entity_id' => $medEval['id'],
+    ));
+
+    $getfiles = $this->callAPISuccess('Case', 'getfiles', array(
+      'case_id' => $cases[0]['id'],
+      'options' => array(
+        'xref' => 1,
+      ),
+    ));
+    $this->assertEquals(1, $getfiles['count']);
+    $attId = $attachment['id'];
+    $this->assertEquals($cases[0]['id'], $getfiles['values'][$attId]['case_id']);
+    $this->assertEquals($medEval['id'], $getfiles['values'][$attId]['activity_id']);
+    $this->assertEquals($attachment['id'], $getfiles['values'][$attId]['id']);
+
+    $this->assertEquals('The subject', $getfiles['xref']['activity'][$medEval['id']]['subject']);
+    $this->assertEquals('The details', $getfiles['xref']['activity'][$medEval['id']]['details']);
+    $this->assertEquals(self::getFilePrefix() . 'TheFile.txt', $getfiles['xref']['file'][$attId]['name']);
+    $this->assertEquals('Hello world', $getfiles['xref']['case'][$cases[0]['id']]['subject']);
+  }
+
+  public function testEmpty() {
+    $this->callAPIFailure('Case', 'getfiles', array());
+  }
+
+}
