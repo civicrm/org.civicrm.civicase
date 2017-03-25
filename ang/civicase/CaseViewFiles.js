1ddf8693e981cacddaf48fa214a5fa73f3562790
Tim Otten <totten@civicrm.org>
2017-03-24 22:05:47 -0700

    CaseViewFiles - Load filtered file list

--- ang/civicase/CaseViewFiles.js
+++ ang/civicase/CaseViewFiles.js
@@ -1,8 +1,24 @@
 (function(angular, $, _) {
 
-  function caseFilesController($scope, crmApi) {
+  function caseFilesController($scope, crmThrottle, crmApi) {
     var ts = $scope.ts = CRM.ts('civicase'),
       item = $scope.item;
+
+    $scope.fileFilters = {text: ''};
+    $scope.fileData = {};
+    function refresh() {
+      crmThrottle(function(){
+        return crmApi('Case', 'getfiles', {
+          case_id: item.id,
+          text: $scope.fileFilters.text,
+          options: {xref: 1}
+        })
+          .then(function(response){
+            $scope.fileData = response;
+          });
+      });
+    }
+    $scope.$watchCollection("fileFilters", refresh);
   }
 
   angular.module('civicase').directive('civicaseViewFiles', function() {
