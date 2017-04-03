6b35955d2cbee3edbee525b5ba60005ea4b73b8d
Coleman Watts <coleman@civicrm.org>
2017-04-02 23:46:52 -0400

    Add basic case filters

--- ang/civicase/CaseList.js
+++ ang/civicase/CaseList.js
@@ -17,10 +17,23 @@
     $scope.caseIsFocused = false;
     $scope.sortField = 'contact_id.sort_name';
     $scope.sortDir = 'ASC';
+    $scope.filters = {};
+
+    function mapSelectOptions(opt) {
+      return {
+        id: opt.value || opt.name,
+        text: opt.label || opt.title,
+        color: opt.color,
+        icon: opt.icon
+      };
+    }
 
     var caseTypes = CRM.civicase.caseTypes;
     var caseStatuses = CRM.civicase.caseStatuses;
     $scope.activityTypes = CRM.civicase.activityTypes;
+
+    $scope.caseTypeOptions = _.map(caseTypes, mapSelectOptions);
+    $scope.caseStatusOptions = _.map(caseStatuses, mapSelectOptions);
 
     $scope.cases = [];
 
@@ -122,6 +135,15 @@
       var params = {
         is_deleted: 0
       };
+      _.each($scope.filters, function(val, filter) {
+        if (val && val.length) {
+          params[filter] = {IN: val.split(',')};
+        }
+      });
+      // If no status specified, default to all open cases
+      if (!params.status_id) {
+        params['status_id.grouping'] = 'Opened';
+      }
       return crmApi({
         cases: ['Case', 'getdetails', $.extend(true, returnParams, params)],
         count: ['Case', 'getcount', params]
@@ -130,6 +152,7 @@
 
     $scope.$watch('sortField', getCases);
     $scope.$watch('sortDir', getCases);
+    $scope.$watchCollection('filters', getCases);
 
   });
 
