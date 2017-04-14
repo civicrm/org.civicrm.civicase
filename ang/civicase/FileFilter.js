d5b83007468420e64eaf0ecf6e9eaa9e5e8ce67b
Tim Otten <totten@civicrm.org>
2017-04-13 22:50:11 -0700

    FileFilter - Add "Show letters" checkbox

--- ang/civicase/FileFilter.js
+++ ang/civicase/FileFilter.js
@@ -3,6 +3,17 @@
   function FileFilterCtrl($scope) {
     var ts = $scope.ts = CRM.ts('civicase');
     $scope.fileCategoriesIT = CRM.civicase.fileCategories;
+    $scope.customFilters = {
+      showLetters: false
+    };
+    $scope.$watchCollection('customFilters', function() {
+      if ($scope.customFilters.showLetters) {
+        $scope.apiCtrl.params.activity_type_id = 'Print PDF Letter';
+      }
+      else {
+        delete $scope.apiCtrl.params.activity_type_id;
+      }
+    });
   }
 
   angular.module('civicase').directive('civicaseFileFilter', function() {
