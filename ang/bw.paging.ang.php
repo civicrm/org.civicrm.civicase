7ab264a726c12848755657b1dde034d779ca928c
Tim Otten <totten@civicrm.org>
2017-05-22 19:28:09 -0700

    bw.paging - Declare via *.ang.php

---
+++ ang/bw.paging.ang.php
@@ -0,0 +1,4 @@
+<?php
+return array(
+  'js' => array('packages/paging.min.js'),
+);
--- ang/civicase.ang.php
+++ ang/civicase.ang.php
@@ -4,7 +4,6 @@
 // http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
 Civi::resources()
   ->addPermissions(array('administer CiviCase', 'administer CiviCRM'))
-  ->addScriptFile('org.civicrm.civicase', 'packages/paging.min.js', 140, 'html-header')
   ->addSetting(array(
     'config' => array(
       'enableComponents' => CRM_Core_Config::singleton()->enableComponents,
