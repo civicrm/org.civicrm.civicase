fa46d03086ea37cf9fe75e1d56a79d906deceecd
Tim Otten <totten@civicrm.org>
2017-06-22 00:43:06 -0700

    CRM-20756 - Dashboard - Add screen `/case` with dashboard and tabs

---
+++ ang/civicase/DashboardCtrl.js
@@ -0,0 +1,22 @@
+(function(angular, $, _) {
+
+  angular.module('civicase').config(function($routeProvider) {
+      $routeProvider.when('/case', {
+        reloadOnSearch: false,
+        controller: 'CivicaseDashboardCtrl',
+        templateUrl: '~/civicase/DashboardCtrl.html'
+      });
+    }
+  );
+
+  angular.module('civicase').controller('CivicaseDashboardCtrl', function($scope) {
+    var ts = $scope.ts = CRM.ts('civicase');
+    $scope.$bindToRoute({
+      param: 'dtab',
+      expr: 'activeTab',
+      format: 'json',
+      default: 0
+    });
+  });
+
+})(angular, CRM.$, CRM._);
