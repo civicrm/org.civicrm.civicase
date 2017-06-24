aa06db621b13e7a7e0180dd001d5eca8d6631dd2
Tim Otten <totten@civicrm.org>
2017-06-23 22:20:02 -0700

    CRM-20756 - DashboardCtrl - Plugin "my" case IDs

--- ang/civicase/DashboardCtrl.js
+++ ang/civicase/DashboardCtrl.js
@@ -9,7 +9,7 @@
     }
   );
 
-  angular.module('civicase').controller('CivicaseDashboardCtrl', function($scope) {
+  angular.module('civicase').controller('CivicaseDashboardCtrl', function($scope, crmApi, crmStatus) {
     var ts = $scope.ts = CRM.ts('civicase');
     $scope.$bindToRoute({
       param: 'dtab',
@@ -23,6 +23,35 @@
       expr: 'myCasesOnly',
       format: 'bool',
       default: false
+    });
+
+    // @return Promise<Array<int>>
+    function getMyCaseIds() {
+      return crmApi('Case', 'getdetails', {
+        is_deleted: 0,
+        options: {
+          offset: 0,
+          limit: 0
+        },
+        return: ['id'],
+        case_manager: {'IN': [CRM.config.user_contact_id]}
+      }).then(function(r){
+        return _.collect(r.values, 'id');
+      });
+    }
+
+    // Translate between the dashboard's global filter-options and
+    // the narrower, per-section filter-options.
+    $scope.$watch('myCasesOnly', function (myCasesOnly) {
+      if (myCasesOnly) {
+        // Hmm, this might be a good idea, but none of CiviCase UI does it: crmStatus(ts('Refreshing'), ...);
+        getMyCaseIds().then(function (caseIds) {
+          $scope.activityFilters = {case_id: {'IN': caseIds}};
+        })
+      }
+      else {
+        $scope.activityFilters = {case_id: {'IS NOT NULL': 1}};
+      }
     });
   });
 
