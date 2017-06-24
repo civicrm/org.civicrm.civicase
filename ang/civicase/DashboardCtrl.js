e862c1b37c1a2962ce516f03c1d6787705611e63
Tim Otten <totten@civicrm.org>
2017-06-23 23:09:16 -0700

    CRM-20756 - DashboardCtrl - Reduce AJAX calls
    To implement the "My cases only" filter for activities, we're using a trick
    of "get all the case ID's in my purview".  The previous implementation was
    client-side, leading to an extra round-tirp with a variable-length dataset.
    
    This revision implements the trick on the server-side.

--- ang/civicase/DashboardCtrl.js
+++ ang/civicase/DashboardCtrl.js
@@ -25,29 +25,13 @@
       default: false
     });
 
-    // @return Promise<Array<int>>
-    function getMyCaseIds() {
-      return crmApi('Case', 'getdetails', {
-        is_deleted: 0,
-        options: {
-          offset: 0,
-          limit: 0
-        },
-        return: ['id'],
-        case_manager: {'IN': [CRM.config.user_contact_id]}
-      }).then(function(r){
-        return _.collect(r.values, 'id');
-      });
-    }
-
     // Translate between the dashboard's global filter-options and
     // the narrower, per-section filter-options.
     $scope.$watch('myCasesOnly', function (myCasesOnly) {
       if (myCasesOnly) {
-        // Hmm, this might be a good idea, but none of CiviCase UI does it: crmStatus(ts('Refreshing'), ...);
-        getMyCaseIds().then(function (caseIds) {
-          $scope.activityFilters = {case_id: {'IN': caseIds}};
-        })
+        $scope.activityFilters = {
+          case_filter: {case_manager: CRM.config.user_contact_id}
+        };
       }
       else {
         $scope.activityFilters = {case_id: {'IS NOT NULL': 1}};
