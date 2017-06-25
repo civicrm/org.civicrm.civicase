d63bd0f235ce3ae52d2fcf395114eb0889b02eb2
Coleman Watts <coleman@civicrm.org>
2017-06-24 22:32:06 -0400

    Improve markup for other cases widget

--- ang/civicase/View.js
+++ ang/civicase/View.js
@@ -24,13 +24,13 @@
           contact_id: {IN: "$value.contact_id"},
           id: {"!=": "$value.id"},
           is_deleted: 0,
-          return: ['case_type_id', 'start_date', 'end_date', 'status_id', 'contacts']
+          return: ['case_type_id', 'start_date', 'end_date', 'status_id', 'contacts', 'subject']
         },
         // Linked cases
         'api.Case.get.2': {
           id: {IN: "$value.related_case_ids"},
           is_deleted: 0,
-          return: ['case_type_id', 'start_date', 'end_date', 'status_id', 'contacts']
+          return: ['case_type_id', 'start_date', 'end_date', 'status_id', 'contacts', 'subject']
         },
         // For the "recent communication" panel
         'api.Activity.get.1': {
