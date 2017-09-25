ca1540be43c6c190f931ae888f197faa8c5ceac0
Tim Otten <totten@civicrm.org>
2017-09-25 13:38:38 -0700

    CRM-21179 - TypeDurationChart - Use more realistic averaging

--- ang/civicase/TypeDurationChart.js
+++ ang/civicase/TypeDurationChart.js
@@ -15,7 +15,12 @@
           var chart = dc.rowChart($el[0]),
             ndx                 = crossfilter(data),
             caseTypeDimension   = ndx.dimension(function(d) {return d['case_type_id.title'];}),
-            countGroup          = caseTypeDimension.group().reduceSum(function(d) {return d.count;});
+            countGroup          = caseTypeDimension.group().reduce(
+              function onAdd(p,v) { p.count += v.count; p.total += (v.count * v.average_duration); return p;},
+              function onRemove(p,v) { p.count -= v.count; p.total -= (v.count * v.average_duration); return p; },
+              function onInit() {return {count: 0, total: 0}; }
+            );
+          function avg(p) { return (p.count) ? Math.round(p.total / p.count) : 0; }
 
           chart
             .width(300)
@@ -24,7 +29,8 @@
             .colors(d3.scale.category20())
             .dimension(caseTypeDimension)
             .group(countGroup)
-            .title(function(d){return ts('%1 days', {1: d.value});})
+            .valueAccessor(function(d) { return avg(d.value); } )
+            .title(function(d){return ts('%1 days', {1: avg(d.value)});})
             .render();
 
           chart.onClick = function(){};
@@ -37,13 +43,15 @@
           // crmApi('Case', 'gettypestats', params).then(function(response){
           //   var data = response.values;
           var data = [
-            {'case_type_id.title': 'Fixme', 'count': 32.1},
-            {'case_type_id.title': 'Todo', 'count': 15.4},
-            {'case_type_id.title': 'Blank', 'count': 13.2},
-            {'case_type_id.title': 'Placeholder', 'count': 19.7}
+            {'case_type_id.title': 'Fixme', 'count': 5, average_duration: 32.7},
+            {'case_type_id.title': 'Fixme', 'count': 7, average_duration: '12.25'},
+            {'case_type_id.title': 'Todo', 'count': 19, average_duration: '15.4'},
+            {'case_type_id.title': 'Blank', 'count': 30, average_duration: 13.2},
+            {'case_type_id.title': 'Placeholder', 'count': 11, average_duration: '19.7'}
           ];
             data.forEach(function(x) {
               x.count = +x.count;
+              x.average_duration = +x.average_duration;
             });
             fullDraw(data);
           // });
