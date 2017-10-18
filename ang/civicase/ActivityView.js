e220bada36507beb84fbe3947d52474cf6ad61ef
Coleman Watts <coleman@civicrm.org>
2017-10-18 12:19:25 -0400

    Fix display of file on case button

--- ang/civicase/ActivityView.js
+++ ang/civicase/ActivityView.js
@@ -3,7 +3,8 @@
   function activityView(scope, element, attrs) {
     function loadActivity() {
       if (scope.activity.id) {
-        CRM.loadForm(CRM.url('civicrm/activity', {action: 'view', id: scope.activity.id, reset: 1}), {target: $(element).children('div.act-view-container')});
+        var context = scope.activity.case_id ? 'case' : 'activity';
+        CRM.loadForm(CRM.url('civicrm/activity', {action: 'view', id: scope.activity.id, reset: 1, context: context}), {target: $(element).children('div.act-view-container')});
       }
     }
     scope.close = function() {
