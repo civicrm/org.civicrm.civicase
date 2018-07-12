4aa6ed96cbb03438b9e63c5a97cb1d6f09f7ebfa
Swastik Pareek <swastik1608@gmail.com>
2018-07-12 11:47:47 +0530

    SHOR-5: Fix screen for Manage case

--- tests/backstop_data/engine_scripts/puppet/civicase/manage.js
+++ tests/backstop_data/engine_scripts/puppet/civicase/manage.js
@@ -2,21 +2,27 @@
 
 module.exports = async (engine, scenario, vp) => {
   await require('./find.js')(engine, scenario, vp);
-  await Promise.all([
-    engine.click('a[title="Manage Case"]'),
-    engine.waitForNavigation()
-  ]);
+  await engine.goto(await buildUrl.call(this, engine, 'a[title="Manage Case"]'), {'waitUntil': 'networkidle0'});
+  // open all accordions
   await engine.evaluate(selector => {
     document.querySelectorAll(selector).forEach(element => element.click());
   }, 'div.crm-accordion-wrapper.collapsed > div');
-  try {
-    await this.engine.waitFor('.blockUI.blockOverlay', { hidden: true });
-    await this.engine.waitFor('.loading-text', { hidden: true, timeout: 8000 });
-    await this.engine.waitFor('[alt="loading"]', { hidden: true });
-    // wait for reedjustment of the modal after ajax content load after opening accordion
-    await this.engine.waitFor(500);
-    console.log('All accordion blocks loaded');
-  } catch (e) {
-    console.log('Loaders still visible and timeout reached!');
-  }  
 };
+
+/**
+  * Builds full absolute url for the new page to be navigated to
+  *
+  * @param {Object} engine - engine object of puppeter
+  * @param {String} selector - <a> tag selector to be clicked on for navigation
+  *
+  * @return {String} - Full absolute url for Manage cases page
+  */
+async function buildUrl (engine, selector) {
+  const urlRegex = /^(.*:)\/\/([A-Za-z0-9\-.]+)(:[0-9]+)?(.*)$/g;
+  const urlElements = urlRegex.exec(engine.url());
+  const relativeUrl = await engine.evaluate((selector) => {
+    return document.querySelector(selector).getAttribute('href');
+  }, selector);
+
+  return urlElements[1] + '//' + urlElements[2] + (urlElements[3] !== undefined ? urlElements[3] : '') + relativeUrl;
+}
