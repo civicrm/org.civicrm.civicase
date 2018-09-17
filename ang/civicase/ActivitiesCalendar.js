f201ae36aeb91b5bc6f87d2515982d2b8af33128
René Olivo <minet.ws@gmail.com>
2018-09-17 03:19:30 +0000

    C51-66: Store activities for the selected date

--- ang/civicase/ActivitiesCalendar.js
+++ ang/civicase/ActivitiesCalendar.js
@@ -14,5 +14,19 @@
     var vm = this;
 
     vm.calendarOptions = { showWeeks: false };
+    vm.selectedActivites = [];
+    vm.selectedDate = null;
+
+    vm.onDateSelected = onDateSelected;
+
+    /**
+     * It stores the activities that are on the same date as the calendar's
+     * selected date. Triggers when the calendar date changes.
+     */
+    function onDateSelected () {
+      vm.selectedActivites = vm.activities.filter(function (activity) {
+        return moment(activity.activity_date_time).isSame(vm.selectedDate, 'day');
+      });
+    }
   }
 })(angular);
--- ang/test/civicase/ActivitiesCalendar.spec.js
+++ ang/test/civicase/ActivitiesCalendar.spec.js
@@ -1,21 +1,69 @@
 /* eslint-env jasmine */
 
-describe('ActivitiesCalendar', function () {
-  var $componentController, activitiesCalendar;
+(function (moment) {
+  describe('ActivitiesCalendar', function () {
+    var $componentController, activitiesCalendar, activitiesMockData, dates;
 
-  beforeEach(module('civicase'));
+    beforeEach(module('civicase', 'civicase.data'));
 
-  beforeEach(inject(function (_$componentController_) {
-    $componentController = _$componentController_;
-  }));
-
-  beforeEach(function () {
-    activitiesCalendar = $componentController('civicaseActivitiesCalendar');
-  });
-
-  it('hides the weeks panel from the calendar', function () {
-    expect(activitiesCalendar.calendarOptions).toEqual(jasmine.objectContaining({
-      showWeeks: false
+    beforeEach(inject(function (_$componentController_, _activitiesMockData_, datesMockData) {
+      $componentController = _$componentController_;
+      activitiesMockData = _activitiesMockData_.get();
+      dates = datesMockData;
     }));
+
+    describe('calendar options', function () {
+      beforeEach(function () {
+        initComponent();
+      });
+
+      it('hides the weeks panel from the calendar', function () {
+        expect(activitiesCalendar.calendarOptions).toEqual(jasmine.objectContaining({
+          showWeeks: false
+        }));
+      });
+    });
+
+    describe('selected activities', function () {
+      beforeEach(function () {
+        initComponent();
+      });
+
+      describe('when selecting a date with activities included', function () {
+        var expectedSelectedActivities;
+
+        beforeEach(function () {
+          expectedSelectedActivities = activitiesMockData.filter(function (activity) {
+            return moment(activity.activity_date_time).isSame(dates.today, 'day');
+          });
+
+          activitiesCalendar.selectedDate = dates.today;
+          activitiesCalendar.onDateSelected();
+        });
+
+        it('only provides the activities for the given date', function () {
+          expect(activitiesCalendar.selectedActivites).toEqual(expectedSelectedActivities);
+        });
+      });
+
+      describe('when selecting a date with no activities included', function () {
+        beforeEach(function () {
+          activitiesCalendar.selectedDate = dates.nextWeek;
+          activitiesCalendar.onDateSelected();
+        });
+
+        it('only provides the activities for the given date', function () {
+          expect(activitiesCalendar.selectedActivites).toEqual([]);
+        });
+      });
+    });
+
+    function initComponent (activities) {
+      activities = activities || [];
+
+      activitiesCalendar = $componentController('civicaseActivitiesCalendar', null, {
+        activities: activitiesMockData
+      });
+    }
   });
-});
+})(moment);
