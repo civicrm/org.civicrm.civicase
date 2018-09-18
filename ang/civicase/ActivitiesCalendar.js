76ea0b3ee55d9d5c63fb10a3135692fc2a4e83ac
René Olivo <minet.ws@gmail.com>
2018-09-18 10:55:47 +0000

    C51-66: Move activities calendar link function so it is inside directive definition

--- ang/civicase/ActivitiesCalendar.js
+++ ang/civicase/ActivitiesCalendar.js
@@ -12,12 +12,64 @@
       bindToController: true,
       templateUrl: '~/civicase/ActivitiesCalendar.html',
       restrict: 'E',
-      link: function ($scope, element) {
-        civicaseActivitiesCalendarDomEvents({
-          $scope: $scope,
-          $uibPosition: $uibPosition,
-          element: element
-        });
+      /**
+       * AngularJS's link function for the civicase activity calendar directive.
+       *
+       * @param {Object} $scope
+       * @param {Object} element
+       */
+      link: function civicaseActivitiesCalendarLink ($scope, element) {
+        var bootstrapThemeContainer = $('#bootstrap-theme');
+        var popover = element.find('.activities-calendar-popover');
+
+        (function init () {
+          $scope.$on('civicaseActivitiesCalendar::openActivitiesPopover', openActivitiesPopover);
+        })();
+
+        /**
+         * Closes the activities dropdown but only when clicking outside the popover
+         * container. Also unbinds the mouseup event in order to reduce the amount
+         * of active DOM event listeners.
+         */
+        function closeActivitiesDropdown (event) {
+          // Note: it breaks when checking `popover.is(event.target)`.
+          var isNotPopover = !$(event.target).is('.activities-calendar-popover');
+          var notInsidePopover = popover.has(event.target).length === 0;
+
+          if (isNotPopover && notInsidePopover) {
+            popover.hide();
+            $(document).unbind('mouseup', closeActivitiesDropdown);
+          }
+        }
+
+        /**
+         * Displays the popover on top of the calendar's current active day.
+         */
+        function displayPopoverOnTopOfActiveDay () {
+          // the current active day can only be determined in the next cicle:
+          setTimeout(function () {
+            var activeDay = element.find('.uib-day .active');
+            var bodyOffset = {};
+
+            popover.show();
+            popover.appendTo(bootstrapThemeContainer);
+
+            bodyOffset = $uibPosition.positionElements(activeDay, popover, 'bottom', true);
+            popover.css({
+              top: bodyOffset.top - bootstrapThemeContainer.offset().top,
+              left: bodyOffset.left - bootstrapThemeContainer.offset().left
+            });
+          });
+        }
+
+        /**
+         * Opens up the activitis popover and binds the mouseup event in order
+         * to close the popover.
+         */
+        function openActivitiesPopover () {
+          displayPopoverOnTopOfActiveDay();
+          $(document).bind('mouseup', closeActivitiesDropdown);
+        }
       }
     };
   }]);
@@ -51,72 +103,6 @@
       if (vm.selectedActivites.length) {
         $scope.$emit('civicaseActivitiesCalendar::openActivitiesPopover');
       }
-    }
-  }
-
-  /**
-   * Attaches events to the DOM for opening, hiding, and position the activities
-   * calendar popover.
-   *
-   * @param {Object} dependencies
-   * @param {Object} dependencies.$uibPosition - the $uibPosition service.
-   * @param {Object} dependencies.$scope - the $scope where the directive is attached to.
-   * @param {Object} dependencies.element - the element where the directive is attached to.
-   */
-  function civicaseActivitiesCalendarDomEvents (dependencies) {
-    var $uibPosition = dependencies.$uibPosition;
-    var $scope = dependencies.$scope;
-    var bootstrapThemeContainer = $('#bootstrap-theme');
-    var element = dependencies.element;
-    var popover = element.find('.activities-calendar-popover');
-
-    (function init () {
-      $scope.$on('civicaseActivitiesCalendar::openActivitiesPopover', openActivitiesPopover);
-    })();
-
-    /**
-     * Closes the activities dropdown but only when clicking outside the popover
-     * container. Also unbinds the mouseup event in order to reduce the amount
-     * of active DOM event listeners.
-     */
-    function closeActivitiesDropdown (event) {
-      // Note: it breaks when checking `popover.is(event.target)`.
-      var isNotPopover = !$(event.target).is('.activities-calendar-popover');
-      var notInsidePopover = popover.has(event.target).length === 0;
-
-      if (isNotPopover && notInsidePopover) {
-        popover.hide();
-        $(document).unbind('mouseup', closeActivitiesDropdown);
-      }
-    }
-
-    /**
-     * Displays the popover on top of the calendar's current active day.
-     */
-    function displayPopoverOnTopOfActiveDay () {
-      // the current active day can only be determined in the next cicle:
-      setTimeout(function () {
-        var activeDay = element.find('.uib-day .active');
-        var bodyOffset = {};
-
-        popover.show();
-        popover.appendTo(bootstrapThemeContainer);
-
-        bodyOffset = $uibPosition.positionElements(activeDay, popover, 'bottom', true);
-        popover.css({
-          top: bodyOffset.top - bootstrapThemeContainer.offset().top,
-          left: bodyOffset.left - bootstrapThemeContainer.offset().left
-        });
-      });
-    }
-
-    /**
-     * Opens up the activitis popover and binds the mouseup event in order
-     * to close the popover.
-     */
-    function openActivitiesPopover () {
-      displayPopoverOnTopOfActiveDay();
-      $(document).bind('mouseup', closeActivitiesDropdown);
     }
   }
 })(CRM.$, angular);
