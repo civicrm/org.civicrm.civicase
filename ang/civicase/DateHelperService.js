af96d24eded0ec6e48f403a8b0c33255dd9c4b26
Swastik Pareek <swastik1608@gmail.com>
2018-09-27 19:07:34 +0530

    C51-227: Removed isOverdue function from datehelper

--- ang/civicase/DateHelperService.js
+++ ang/civicase/DateHelperService.js
@@ -5,18 +5,6 @@
 
   function DateHelper () {
     /**
-     * To check if the date is overdue
-     *
-     * @param {Object} activity object
-     * @return {Boolean} if the date is overdue.
-     */
-    this.isOverdue = function (activity) {
-      if (typeof activity.is_overdue !== 'undefined') {
-        return typeof (activity.is_overdue) !== 'boolean' ? activity.is_overdue === '1' : activity.is_overdue;
-      }
-    };
-
-    /**
      * Formats Date in sent format
      * Default format is (DD/MM/YYYY)
      *
