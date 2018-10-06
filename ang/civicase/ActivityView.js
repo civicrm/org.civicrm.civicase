91668ac046740dc18ed97a827a0bde3134f656d3
Debarshi Bhaumik <deb1990@gmail.com>
2018-10-06 10:38:09 +0530

    C51-120: Code refactor

--- ang/civicase/ActivityView.js
+++ ang/civicase/ActivityView.js
@@ -1,51 +1,11 @@
-(function(angular, $, _) {
+(function (angular, $, _) {
+  var module = angular.module('civicase');
 
-  function activityView(scope, element, attrs) {
-    function loadActivity() {
-      if (scope.activity.id) {
-        var context = scope.activity.case_id ? 'case' : 'activity';
-        CRM.loadForm(CRM.url('civicrm/activity', {action: 'view', id: scope.activity.id, reset: 1, context: context}), {target: $(element).children('div.act-view-container')});
-      }
-    }
-    scope.close = function() {
-      delete(scope.activity.id);
-    };
-    scope.$watch('activity.id', loadActivity);
-
-    element
-      .on('crmFormSuccess', scope.refresh)
-      .on('crmLoad', function() {
-        // Workaround bug where href="#" changes the angular route
-        $('a.crm-clear-link', this).removeAttr('href');
-        $('a.delete.button', this).click(function(e) {
-          CRM.confirm({
-              title: ts('Delete Activity'),
-              message: ts('Permanently delete this %1 activity?', {1: scope.activity.type})
-            })
-            .on('crmConfirm:yes', function() {
-              $(element).children('div.act-view-container').block();
-              CRM.api3('Activity', 'delete', {id: scope.activity.id})
-                .done(scope.close)
-                .done(scope.refresh);
-            });
-          return false;
-        });
-
-        if (CRM.checkPerm('basic case information') &&
-          !CRM.checkPerm('administer CiviCase') &&
-          !CRM.checkPerm('access my cases and activities') &&
-          !CRM.checkPerm('access all cases and activities')
-        ) {
-          $('div.crm-submit-buttons').remove();
-        }
-      });
-  }
-
-  angular.module('civicase').directive('civicaseActivityView', function() {
+  module.directive('civicaseActivityView', function () {
     return {
       restrict: 'A',
       templateUrl: '~/civicase/ActivityView.html',
-      link: activityView,
+      link: civicaseActivityViewLink,
       scope: {
         activity: '=civicaseActivityView',
         refresh: '=refreshCallback'
@@ -53,4 +13,55 @@
     };
   });
 
+  function civicaseActivityViewLink (scope, element, attrs) {
+    var ts = CRM.ts('civicase');
+
+    scope.close = function () {
+      delete scope.activity.id;
+    };
+
+    (function init () {
+      scope.$watch('activity.id', loadActivity);
+      element.on('crmFormSuccess', scope.refresh);
+      element.on('crmLoad', crmLoadListener);
+    }());
+
+    function crmLoadListener () {
+      // Workaround bug where href="#" changes the angular route
+      $('a.crm-clear-link', this).removeAttr('href');
+      $('a.delete.button', this).click(function (e) {
+        CRM.confirm({
+          title: ts('Delete Activity'),
+          message: ts('Permanently delete this %1 activity?', {1: scope.activity.type})
+        })
+          .on('crmConfirm:yes', function () {
+            $(element).children('div.act-view-container').block();
+            CRM.api3('Activity', 'delete', {id: scope.activity.id})
+              .done(scope.close)
+              .done(scope.refresh);
+          });
+        return false;
+      });
+
+      if (CRM.checkPerm('basic case information') &&
+        !CRM.checkPerm('administer CiviCase') &&
+        !CRM.checkPerm('access my cases and activities') &&
+        !CRM.checkPerm('access all cases and activities')
+      ) {
+        $('div.crm-submit-buttons').remove();
+      }
+    }
+
+    function loadActivity () {
+      if (scope.activity.id) {
+        var context = scope.activity.case_id ? 'case' : 'activity';
+        CRM.loadForm(CRM.url('civicrm/activity', {
+          action: 'view',
+          id: scope.activity.id,
+          reset: 1,
+          context: context
+        }), {target: $(element).children('div.act-view-container')});
+      }
+    }
+  }
 })(angular, CRM.$, CRM._);
