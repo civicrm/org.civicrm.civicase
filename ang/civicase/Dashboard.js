dc054fa36dc6cb07398ff1b18712ef0066448e4b
Debarshi Bhaumik <deb1990@gmail.com>
2018-10-15 12:34:05 +0530

    C51-157: Filters Basic Setup

--- ang/civicase/Dashboard.js
+++ ang/civicase/Dashboard.js
@@ -1,66 +1,64 @@
-(function(angular, $, _) {
+(function (angular, $, _) {
+  var module = angular.module('civicase');
 
-  angular.module('civicase').config(function($routeProvider) {
-      $routeProvider.when('/case', {
-        reloadOnSearch: false,
-        controller: 'CivicaseDashboardCtrl',
-        templateUrl: '~/civicase/DashboardCtrl.html'
-      });
-    }
-  );
+  module.config(function ($routeProvider) {
+    $routeProvider.when('/case', {
+      reloadOnSearch: false,
+      controller: 'CivicaseDashboardCtrl',
+      templateUrl: '~/civicase/Dashboard.html'
+    });
+  });
 
-  angular.module('civicase').controller('CivicaseDashboardCtrl', function($scope, crmApi, formatActivity, $timeout) {
-    var ts = $scope.ts = CRM.ts('civicase'),
-      activitiesToShow = 10;
+  module.controller('CivicaseDashboardCtrl', function ($scope, crmApi, formatActivity, $timeout) {
+    var activitiesToShow = 10;
+
+    $scope.ts = CRM.ts('civicase');
     $scope.caseStatuses = CRM.civicase.caseStatuses;
     $scope.caseTypes = CRM.civicase.caseTypes;
     $scope.caseTypesLength = _.size(CRM.civicase.caseTypes);
     $scope.checkPerm = CRM.checkPerm;
     $scope.url = CRM.url;
     $scope.activityPlaceholders = _.range(activitiesToShow);
-
-    $scope.$bindToRoute({
-      param: 'dtab',
-      expr: 'activeTab',
-      format: 'int',
-      default: 0
-    });
-
-    if (CRM.checkPerm('access all cases and activities')) {
-      $scope.$bindToRoute({
-        param: 'dme',
-        expr: 'myCasesOnly',
-        format: 'bool',
-        default: false
-      });
-    } else {
-      $scope.myCasesOnly = true;
-    }
-
-    $scope.$bindToRoute({
-      param: 'dbd',
-      expr: 'showBreakdown',
-      format: 'bool',
-      default: false
-    });
-
-    // We hide the breakdown when there's only one case type
-    if ($scope.caseTypesLength < 2) {
-      $scope.showBreakdown = false;
-    }
-
     $scope.summaryData = [];
-
     $scope.dashboardActivities = {};
 
-    $scope.showHideBreakdown = function() {
+    (function init () {
+      bindRouteParams();
+      prepareCaseFilterOption();
+
+      // We hide the breakdown when there's only one case type
+      if ($scope.caseTypesLength < 2) {
+        $scope.showBreakdown = false;
+      }
+    }());
+
+    function prepareCaseFilterOption () {
+      var options = [
+        { 'text': 'My cases', 'id': 'is_case_manager' },
+        { 'text': 'Cases I am involved', 'id': 'is_involved' }
+      ];
+
+      if (CRM.checkPerm('access all cases and activities')) {
+        options.push({ 'text': 'All Cases', 'id': 'all' });
+      }
+
+      $scope.caseRelationshipOptions = options;
+    }
+
+    function bindRouteParams () {
+      $scope.$bindToRoute({ param: 'dtab', expr: 'activeTab', format: 'int', default: 0 });
+      $scope.$bindToRoute({ param: 'dbd', expr: 'showBreakdown', format: 'bool', default: false });
+      $scope.$bindToRoute({ param: 'drel', expr: 'caseRelationshipType', format: 'raw', default: 'is_involved' });
+    }
+
+    $scope.showHideBreakdown = function () {
       $scope.showBreakdown = !$scope.showBreakdown;
     };
 
-    $scope.seeAllLink = function(category, statusFilter) {
+    $scope.seeAllLink = function (category, statusFilter) {
       var params = {
         dtab: 1,
-        dme: $scope.myCasesOnly ? 1 : 0,
+        drel: $scope.caseRelationshipType,
         dbd: 0,
         af: JSON.stringify({
           'activity_type_id.grouping': category,
@@ -70,7 +68,7 @@
       return '#/case?' + $.param(params);
     };
 
-    $scope.caseListLink = function(type, status) {
+    $scope.caseListLink = function (type, status) {
       var cf = {};
       if (type) {
         cf.case_type_id = [type];
@@ -84,7 +82,7 @@
       return '#/case/list?' + $.param({cf: JSON.stringify(cf)});
     };
 
-    $scope.refresh = function(apiCalls) {
+    $scope.refresh = function (apiCalls) {
       apiCalls = apiCalls || [];
       apiCalls.push(['Case', 'getstats', {my_cases: $scope.myCasesOnly}]);
       var params = _.extend({
@@ -95,29 +93,29 @@
       }, $scope.activityFilters);
       // recent communication
       apiCalls.push(['Activity', 'get', _.extend({
-        "activity_type_id.grouping": {LIKE: "%communication%"},
+        'activity_type_id.grouping': {LIKE: '%communication%'},
         'status_id.filter': 1,
         options: {limit: activitiesToShow, sort: 'activity_date_time DESC'}
       }, params)]);
       apiCalls.push(['Activity', 'getcount', _.extend({
-        "activity_type_id.grouping": {LIKE: "%communication%"},
+        'activity_type_id.grouping': {LIKE: '%communication%'},
         'status_id.filter': 1,
         is_current_revision: 1,
         is_test: 0
       }, $scope.activityFilters)]);
       // next milestones
       apiCalls.push(['Activity', 'get', _.extend({
-        "activity_type_id.grouping": {LIKE: "%milestone%"},
+        'activity_type_id.grouping': {LIKE: '%milestone%'},
         'status_id.filter': 0,
         options: {limit: activitiesToShow, sort: 'activity_date_time ASC'}
       }, params)]);
       apiCalls.push(['Activity', 'getcount', _.extend({
-        "activity_type_id.grouping": {LIKE: "%milestone%"},
+        'activity_type_id.grouping': {LIKE: '%milestone%'},
         'status_id.filter': 0,
         is_current_revision: 1,
         is_test: 0
       }, $scope.activityFilters)]);
-      crmApi(apiCalls).then(function(data) {
+      crmApi(apiCalls).then(function (data) {
         $scope.$broadcast('caseRefresh');
         $scope.summaryData = data[apiCalls.length - 5].values;
         $scope.dashboardActivities.recentCommunication = _.each(data[apiCalls.length - 4].values, formatActivity);
@@ -131,7 +129,7 @@
     // the narrower, per-section filter-options.
     $scope.$watch('myCasesOnly', function (myCasesOnly) {
       $scope.activityFilters = {
-        case_filter: {"case_type_id.is_active": 1, contact_is_deleted: 0}
+        case_filter: {'case_type_id.is_active': 1, contact_is_deleted: 0}
       };
       var recentCaseFilter = {
         'status_id.grouping': 'Opened'
@@ -149,28 +147,24 @@
   // TODO: Move this to common crm-affix directive
   angular.module('civicase').directive('uibTabsetAffix', function ($timeout) {
     return {
-      link: function(scope, $el, attrs) {
-        $timeout(function() {
-
+      link: function (scope, $el, attrs) {
+        $timeout(function () {
           // $el it self is not ul.nav (consider this when creating common directive)
-          var $tabNavigation = $('ul.nav'),
-          $civicrmMenu = $('#civicrm-menu'),
-          $tabContainer = $('.dashboard-tab-container');
+          var $tabNavigation = $('ul.nav');
+          var $civicrmMenu = $('#civicrm-menu');
+          var $tabContainer = $('.dashboard-tab-container');
 
           $tabNavigation.affix({
             offset: {
               top: $tabContainer.offset().top - 128
             }
-          })
-          .on('affixed.bs.affix', function() {
+          }).on('affixed.bs.affix', function () {
             $tabNavigation.css('top', $civicrmMenu.height());
-          })
-          .on('affixed-top.bs.affix', function() {
-            $tabNavigation.css('top','auto');
+          }).on('affixed-top.bs.affix', function () {
+            $tabNavigation.css('top', 'auto');
           });
         });
-    }
-  };
-});
-
+      }
+    };
+  });
 })(angular, CRM.$, CRM._);
