516eba0be532b58f6b96c9581c9268fef3c1ce33
varshith89 <vinu@compucorp.co.uk>
2018-10-25 18:21:10 +0000

    CATL-1021: Format Comments (Minor Fix)

--- api/v3/Case/Getwebforms.php
+++ api/v3/Case/Getwebforms.php
@@ -5,7 +5,9 @@
  * This is used for documentation and validation.
  *
  * @param array $spec description of fields supported by this API call
+ *
  * @return void
+ * 
  * @see http://wiki.civicrm.org/confluence/display/CRMDOC/API+Architecture+Standards
  */
 function _civicrm_api3_case_getwebforms_spec(&$spec) {
