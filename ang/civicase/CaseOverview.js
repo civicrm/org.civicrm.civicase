2161d93bbe9d4790dfe9a8a489d0895f09f33451
Swastik Pareek <swastik1608@gmail.com>
2018-11-06 18:30:14 +0530

    C51-289: Writing tests for caseoverview.js

--- ang/civicase/CaseOverview.js
+++ ang/civicase/CaseOverview.js
@@ -1,6 +1,6 @@
 (function (angular, $, _) {
   var module = angular.module('civicase');
-  // Todo: To write unit tests
+
   module.directive('civicaseCaseOverview', function () {
     return {
       restrict: 'EA',
@@ -10,72 +10,6 @@
       scope: {},
       link: civicaseCaseOverviewLink
     };
-
-    /**
-     * Controller for civicaseCaseOverview
-     *
-     * @param {Object} $scope
-     * @param {crmApi} Object
-     */
-    function civicaseCaseOverviewController ($scope, crmApi) {
-      $scope.caseStatuses = CRM.civicase.caseStatuses;
-      $scope.caseTypes = CRM.civicase.caseTypes;
-      $scope.caseTypesLength = _.size(CRM.civicase.caseTypes);
-      $scope.summaryData = [];
-
-      (function init () {
-        // We hide the breakdown when there's only one case type
-        if ($scope.caseTypesLength < 2) {
-          $scope.showBreakdown = false;
-        }
-
-        loadStatsData();
-      }());
-
-      /**
-       * Creates link to the filtered cases list
-       *
-       * @param {String} type
-       * @param {String} status
-       * @return {String} link to the filtered list of cases
-       */
-      $scope.caseListLink = function (type, status) {
-        var cf = {};
-
-        if (type) {
-          cf.case_type_id = [type];
-        }
-
-        if (status) {
-          cf.status_id = [status];
-        }
-
-        if ($scope.myCasesOnly) {
-          cf.case_manager = [CRM.config.user_contact_id];
-        }
-
-        return '#/case/list?' + $.param({cf: JSON.stringify(cf)});
-      };
-
-      /**
-       * Toggle show breakdown dropdown
-       */
-      $scope.showHideBreakdown = function () {
-        $scope.showBreakdown = !$scope.showBreakdown;
-      };
-
-      /**
-       * Loads Stats data
-       */
-      function loadStatsData () {
-        var apiCalls = [];
-
-        apiCalls.push(['Case', 'getstats', {}]);
-        crmApi(apiCalls).then(function (response) {
-          $scope.summaryData = response[0].values;
-        });
-      }
-    }
 
     /**
      * Link function for civicaseCaseOverview
@@ -97,4 +31,72 @@
       }
     }
   });
+
+  module.controller('civicaseCaseOverviewController', civicaseCaseOverviewController);
+
+  /**
+   * Controller for civicaseCaseOverview
+   *
+   * @param {Object} $scope
+   * @param {crmApi} Object
+   */
+  function civicaseCaseOverviewController ($scope, crmApi) {
+    $scope.caseStatuses = CRM.civicase.caseStatuses;
+    $scope.caseTypes = CRM.civicase.caseTypes;
+    $scope.caseTypesLength = _.size(CRM.civicase.caseTypes);
+    $scope.summaryData = [];
+
+    (function init () {
+      // We hide the breakdown when there's only one case type
+      if ($scope.caseTypesLength < 2) {
+        $scope.showBreakdown = false;
+      }
+
+      loadStatsData();
+    }());
+
+    /**
+     * Creates link to the filtered cases list
+     *
+     * @param {String} type
+     * @param {String} status
+     * @return {String} link to the filtered list of cases
+     */
+    $scope.caseListLink = function (type, status) {
+      var cf = {};
+
+      if (type) {
+        cf.case_type_id = [type];
+      }
+
+      if (status) {
+        cf.status_id = [status];
+      }
+
+      if ($scope.myCasesOnly) {
+        cf.case_manager = [CRM.config.user_contact_id];
+      }
+
+      return '#/case/list?' + $.param({cf: JSON.stringify(cf)});
+    };
+
+    /**
+     * Toggle show breakdown dropdown
+     */
+    $scope.showHideBreakdown = function () {
+      $scope.showBreakdown = !$scope.showBreakdown;
+    };
+
+    /**
+     * Loads Stats data
+     */
+    function loadStatsData () {
+      var apiCalls = [];
+
+      apiCalls.push(['Case', 'getstats', {}]);
+      crmApi(apiCalls).then(function (response) {
+        $scope.summaryData = response[0].values;
+      });
+    }
+  }
 })(angular, CRM.$, CRM._);
