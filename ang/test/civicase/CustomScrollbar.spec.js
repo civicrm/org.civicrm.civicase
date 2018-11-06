81478fcb22c5863f739c6590e5ef1d51ebaea1e0
Swastik Pareek <swastik1608@gmail.com>
2018-11-06 19:39:24 +0530

    C51-289: Add tests for CustomScrollbar directive

---
+++ ang/test/civicase/CustomScrollbar.spec.js
@@ -0,0 +1,64 @@
+/* eslint-env jasmine */
+
+(function (_) {
+  describe('civicaseCustomScrollbar', function () {
+    var element, $compile, $rootScope, $scope, SimpleBarOriginalFunction, originalRootScopeOn;
+
+    beforeEach(module('civicase'));
+
+    beforeEach(inject(function (_$compile_, _$rootScope_) {
+      $compile = _$compile_;
+      $rootScope = _$rootScope_;
+      $scope = $rootScope.$new();
+    }));
+
+    describe('initiate SimpleBar', function () {
+      beforeEach(function () {
+        SimpleBarOriginalFunction = SimpleBar;
+        SimpleBar = jasmine.createSpy('SimpleBar');
+      });
+
+      afterEach(function () {
+        SimpleBar = SimpleBarOriginalFunction;
+      });
+
+      beforeEach(function () {
+        compileDirective();
+      });
+
+      it('should call SimpleBar()', function () {
+        expect(SimpleBar).toHaveBeenCalledWith(element[0], $scope.options);
+      });
+    });
+
+    describe('add subscribers', function () {
+      beforeEach(function () {
+        originalRootScopeOn = $rootScope.$on;
+        $rootScope.$on = jasmine.createSpy('rootScopeOn');
+      });
+
+      afterEach(function () {
+        $rootScope.$on = originalRootScopeOn;
+      });
+
+      beforeEach(function () {
+        compileDirective();
+      });
+
+      it('should call $rootScope.$on()', function () {
+        expect($rootScope.$on).toHaveBeenCalledWith('civicase::custom-scrollbar::recalculate', jasmine.any(Function));
+      });
+    });
+
+    /**
+     * Initialise directive
+     */
+    function compileDirective () {
+      $scope.options = {
+        'autoHide': false
+      };
+      element = $compile('<div civicase-custom-scrollbar="{{options}}"></div')($scope);
+      $scope.$digest();
+    }
+  });
+})(CRM._);
