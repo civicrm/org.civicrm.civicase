18e7dcf2acc9b68b88dcebd8d2e86ade88a2d203
Alessandro Verdura <alessandro@compucorp.co.uk>
2018-11-21 17:22:48 +0100

    C51-344: Add support for enforce-time attribute

--- ang/civicase/Utils.js
+++ ang/civicase/Utils.js
@@ -444,52 +444,60 @@
      * Link function for civicaseUiDateRange directive
      *
      * Given that the directive uses crm-ui-datepicker with `time: false`
-     * (that is, the user can't select the time manually), it makes sure that
-     * any selected "from" date is set with the time = 00:00:00
-     * and any selected "to" date with the time = 23:59:59
+     * (that is, the user can't select the time manually), it makes sure that,
+     * if the `enforce-time` attribute is applied, any selected "from" date is
+     * set with the time = 00:00:00 and any selected "to" date with the time = 23:59:59
      *
      * @param {Object} $scope
      * @param {Object} element
      * @param {Object} attrs
      */
     function civicaseUiDateRangeLink ($scope, element, attrs) {
+      var enforceTime = attrs.hasOwnProperty('enforceTime');
+
       // Respond to user interaction with the date widgets
       element.on('change', function (e, context) {
         if (context === 'userInput' || context === 'crmClear') {
           $timeout(function () {
             if ($scope.input.from && $scope.input.to) {
               $scope.data = { BETWEEN: [
-                setDateTimeAsRangeLimit($scope.input.from, 'lower'),
-                setDateTimeAsRangeLimit($scope.input.to, 'upper')
+                setAsRangeLimit($scope.input.from, 'lower'),
+                setAsRangeLimit($scope.input.to, 'upper')
               ] };
             } else if ($scope.input.from) {
-              $scope.data = {'>=': setDateTimeAsRangeLimit($scope.input.from, 'lower')};
+              $scope.data = {'>=': setAsRangeLimit($scope.input.from, 'lower')};
             } else if ($scope.input.to) {
-              $scope.data = {'<=': setDateTimeAsRangeLimit($scope.input.to, 'upper')};
+              $scope.data = {'<=': setAsRangeLimit($scope.input.to, 'upper')};
             } else {
               $scope.data = null;
             }
           });
         }
       });
-    }
 
-    /**
-     * Given a date or datetime, it returns it as the lower (YYYY-MM-DD 00:00:00)
-     * or upper (YYYY-MM-DD 23:59:59) datetime range limit
-     *
-     * @param {String} dateTime
-     *   could be either YYYY-MM-DD or YYYY-MM-DD HH:mm:ss
-     * @param {String} [limit="lower"]
-     *   whether the datetime should be set as the lower or upper limit
-     * @return {String}
-     */
-    function setDateTimeAsRangeLimit (dateTime, limit) {
-      limit = limit !== 'upper' ? 'lower' : limit;
+      /**
+       * Given a date or datetime, it returns it as the lower or upper (depending
+       * on the value of the `limit` argument) date range limit
+       *
+       * If the directive didn't have the `enforce-time` attribute applied, then
+       * it will simply return the original value
+       *
+       * @param {String} dateTime
+       *   could be either YYYY-MM-DD or YYYY-MM-DD HH:mm:ss
+       * @param {String} [limit="lower"]
+       *   whether the datetime should be set as the lower or upper limit
+       * @return {String}
+       */
+      function setAsRangeLimit (dateTime, limit) {
+        var date;
 
-      return dateTime.split(' ')[0] + ' ' + (
-        limit === 'lower' ? '00:00:00' : '23:59:59'
-      );
+        limit = limit !== 'upper' ? 'lower' : limit;
+        date = dateTime.split(' ')[0];
+
+        return !enforceTime
+          ? date
+          : date + ' ' + (limit === 'lower' ? '00:00:00' : '23:59:59');
+      }
     }
   });
 
