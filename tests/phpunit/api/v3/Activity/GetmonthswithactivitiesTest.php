37941546747cc2d8c226bb1cdd06b577f7980a5d
Kator James <kator95@gmail.com>
2018-12-18 09:11:14 +0100

    C51-269: PR-feedback

--- tests/phpunit/api/v3/Activity/GetmonthswithactivitiesTest.php
+++ tests/phpunit/api/v3/Activity/GetmonthswithactivitiesTest.php
@@ -9,25 +9,18 @@ use Civi\Test\TransactionalInterface;
  * This is a generic test class implemented with PHPUnit.
  * @group headless
  */
-class api_v3_Activity_GetmonthswithactivitiesTest extends \PHPUnit_Framework_TestCase implements HeadlessInterface, HookInterface, TransactionalInterface {
+class api_v3_Activity_GetmonthswithactivitiesTest extends BaseHeadlessTest {
 
   /**
-   * Civi\Test has many helpers, like install(), uninstall(), sql(), and sqlFile().
-   * See: https://github.com/civicrm/org.civicrm.testapalooza/blob/master/civi-test.md
-   */
-  public function setUpHeadless() {
-    return \Civi\Test::headless()
-      ->installMe(__DIR__)
-      ->apply();
-  }
-
-  /**
-   * Simple example test case.
-   *
-   * Note how the function name begins with the word "test".
+   * Test function to return expected data model.
    */
   public function testApiExample() {
-    $result = civicrm_api3('Activity', 'Getmonthswithactivities', array('magicword' => 'sesame'));
+    $result = civicrm_api3('Activity', 'Getmonthswithactivities')['values'];
+    $this->assetInternalType('array', $result);
+
+    $first = $result[0];
+    $this->assertArrayHasKey('year', $first);
+    $this->assertArrayHasKey('month', $first);
     $this->assertEquals('Twelve', $result['values'][12]['name']);
   }
 
