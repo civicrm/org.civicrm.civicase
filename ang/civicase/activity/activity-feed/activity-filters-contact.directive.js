compucorp/uk.co.compucorp.civicase

C51-382: Folder structure first draft

ang/civicase/activity/activity-feed/activity-filters-contact.directive.js
@@ -1,0 +1,73 @@
+(function (angular, $, _) {
+  var module = angular.module('civicase');
+
+  module.directive('civicaseActivityFiltersContact', function () {
+    return {
+      restrict: 'AE',
+      replace: true,
+      templateUrl: '~/civicase/activity/activity-card/activity-filters-contact.directive.html',
+      scope: {
+        filters: '=civicaseActivityFiltersContact'
+      },
+      link: civicaseActivityFiltersContactLink
+    };
+
+    /**
+     * Link function for civicaseActivityFiltersContact
+     *
+     * @param {Object} $scope
+     */
+    function civicaseActivityFiltersContactLink ($scope) {
+      $scope.ts = CRM.ts('civicase');
+
+      (function init () {
+        $scope.$watch('filters', filtersWatcher);
+        $scope.$on('civicaseActivityFeed.query', feedQueryListener);
+      }());
+
+      /**
+       * Watch listener for filters
+       */
+      function filtersWatcher () {
+        // Ensure "All" checkbox renders.
+        if ($scope.filters['@involvingContact'] === undefined) {
+          $scope.filters['@involvingContact'] = '';
+        }
+      }
+
+      /**
+       * Subscribe listener for civicaseActivityFeed.query
+       *
+       * @param {Object} event
+       * @param {Object} filters
+       * @param {Object} params
+       * @param {Boolean} reset if it's necessary to delete the previously-set properties
+       */
+      function feedQueryListener (event, filters, params, reset) {
+        if (reset) {
+          delete params.contact_id;
+          delete params.assignee_contact_id;
+          delete params.source_contact_id;
+        }
+
+        switch (filters['@involvingContact']) {
+          case 'myActivities':
+            params.contact_id = 'user_contact_id';
+            break;
+
+          case 'delegated':
+            if (_.isEmpty(params.assignee_contact_id)) {
+              params.assignee_contact_id = {'!=': 'user_contact_id'};
+            }
+            if (_.isEmpty(params.source_contact_id)) {
+              params.source_contact_id = 'user_contact_id';
+            }
+            break;
+
+          default:
+            break;
+        }
+      }
+    }
+  });
+})(angular, CRM.$, CRM._);

ang/civicase/activity/activity-feed/activity-filters.directive.js
@@ -1,0 +1,265 @@
+(function (angular, $, _) {
+  var module = angular.module('civicase');
+
+  module.directive('civicaseActivityFilters', function ($timeout, crmUiHelp) {
+    return {
+      restrict: 'A',
+      scope: {
+        params: '=feedParams',
+        showCheckboxes: '=?',
+        bulkAllowed: '=',
+        caseTimelines: '=',
+        displayedCount: '=',
+        totalCount: '=',
+        filters: '=civicaseActivityFilters',
+        displayOptions: '=displayOptions',
+        selectedActivities: '='
+      },
+      replace: true,
+      templateUrl: '~/civicase/activity/activity-card/activity-filters.directive.html',
+      link: activityFiltersLink,
+      transclude: true
+    };
+
+    /**
+     * Link function for civicaseActivityFilters
+     *
+     * @param {Object} $scope
+     * @param {Object} element
+     */
+    function activityFiltersLink ($scope, element) {
+      var ts = $scope.ts = CRM.ts('civicase');
+
+      $scope.activityCategories = prepareActivityCategories();
+      $scope.availableFilters = prepareAvailableFilters();
+      // Default exposed filters
+      $scope.exposedFilters = {
+        activity_type_id: true,
+        status_id: true,
+        assignee_contact_id: true,
+        tag_id: true,
+        text: true
+      };
+
+      (function init () {
+        // Ensure set filters are also exposed
+        _.each($scope.filters, function (filter, key) {
+          $scope.exposedFilters[key] = true;
+        });
+      }());
+
+      /**
+       * Exposes the selected filter in the UI
+       *
+       * @param {Object} field
+       * @param {Object} $event
+       */
+      $scope.exposeFilter = function (field, $event) {
+        var shown = !$scope.exposedFilters[field.name];
+        if (shown) {
+          // Focus search element when selecting
+          $timeout(function () {
+            var $span = $('[data-activity-filter=' + field.name + ']', element);
+
+            if ($('[crm-entityref], [crm-ui-select]', $span).length) {
+              $('[crm-entityref], [crm-ui-select]', $span).select2('open');
+            } else {
+              $('input:first', $span).focus();
+            }
+          }, 50);
+        } else {
+          // Keep menu open when deselecting
+          $event.stopPropagation();
+          delete $scope.filters[field.name];
+        }
+      };
+
+      /**
+       * Checks if any filter has been applied
+       *
+       * @return {Boolean}
+       */
+      $scope.hasFilters = function () {
+        var result = false;
+
+        _.each($scope.filters, function (value) {
+          if (!_.isEmpty(value)) result = true;
+        });
+
+        return result;
+      };
+
+      /**
+       * Clears all the filters
+       */
+      $scope.clearFilters = function () {
+        _.each(_.keys($scope.filters), function (key) {
+          delete $scope.filters[key];
+        });
+      };
+
+      /**
+       * Prepare Activity Filters
+       *
+       * @return {Array}
+       */
+      function prepareAvailableFilters () {
+        var availableFilters = [
+          {
+            name: 'activity_type_id',
+            label: ts('Activity type'),
+            html_type: 'Select',
+            options: _.map(CRM.civicase.activityTypes, mapSelectOptions)
+          },
+          {
+            name: 'status_id',
+            label: ts('Status'),
+            html_type: 'Select',
+            options: _.map(CRM.civicase.activityStatuses, mapSelectOptions)
+          },
+          {
+            name: 'target_contact_id',
+            label: ts('With'),
+            html_type: 'Autocomplete-Select',
+            entity: 'Contact'
+          },
+          {
+            name: 'assignee_contact_id',
+            label: ts('Assigned to'),
+            html_type: 'Autocomplete-Select',
+            entity: 'Contact'
+          },
+          {
+            name: 'tag_id',
+            label: ts('Tagged'),
+            html_type: 'Autocomplete-Select',
+            entity: 'Tag',
+            api_params: {used_for: {LIKE: '%civicrm_activity%'}}
+          },
+          {
+            name: 'text',
+            label: ts('Contains text'),
+            html_type: 'Text'
+          },
+          {
+            name: 'activity_date_time',
+            label: ts('Activity date'),
+            html_type: 'Select Date'
+          }
+        ];
+
+        if (_.includes(CRM.config.enableComponents, 'CiviCampaign')) {
+          availableFilters.push({
+            name: 'campaign_id', label: ts('Campaign'), html_type: 'Autocomplete-Select', entity: 'Campaign'
+          });
+        }
+        if (CRM.checkPerm('administer CiviCRM')) {
+          availableFilters.push({
+            name: 'is_deleted',
+            label: ts('Deleted Activities'),
+            html_type: 'Select',
+            options: [{id: 1, text: ts('Deleted')}, {id: 0, text: ts('Normal')}]
+          },
+          {
+            name: 'is_test',
+            label: ts('Test Activities'),
+            html_type: 'Select',
+            options: [{id: 1, text: ts('Test')}, {id: 0, text: ts('Normal')}]
+          });
+        }
+
+        availableFilters = availableFilters.concat(CRM.civicase.customActivityFields);
+
+        return availableFilters;
+      }
+
+      /**
+       * Prepare Activity Categories
+       *
+       * @return {Array}
+       */
+      function prepareActivityCategories () {
+        return _.map(CRM.civicase.activityCategories, function (category, key) {
+          category.id = key;
+          category.text = category.label;
+
+          return category;
+        });
+      }
+
+      /**
+       * Maps Options to be used in the dropdown
+       *
+       * @param {Object} option
+       * @param {int/string} id
+       * @return {Object}
+       */
+      function mapSelectOptions (option, id) {
+        return {
+          id: id,
+          text: option.label,
+          color: option.color,
+          icon: option.icon
+        };
+      }
+    }
+  });
+
+  module.directive('civicaseActivityFiltersAffix', function ($rootScope, $timeout) {
+    return {
+      link: civicaseActivityFiltersAffix
+    };
+
+    /**
+     * Link function for civicaseActivityFiltersAffix
+     *
+     * @param {Object} scope
+     * @param {Object} $el
+     * @param {Object} attr
+     */
+    function civicaseActivityFiltersAffix (scope, $el, attr) {
+      var $filter, $feedBodyPanel, $tabs, $toolbarDrawer;
+
+      (function init () {
+        affixActivityFilters();
+        $rootScope.$on('civicase::case-search::dropdown-toggle', resetAffix);
+      }());
+
+      /**
+       * Sets Activity Filters affix offsets
+       */
+      function affixActivityFilters () {
+        $filter = $('.civicase__activity-filter');
+        $feedBodyPanel = $('.civicase__activity-filter ~ .panel-body');
+        $tabs = $('.civicase__dashboard').length > 0 ? $('.civicase__dashboard__tab-container ul.nav') : $('.civicase__case-body_tab');
+        $toolbarDrawer = $('#toolbar');
+
+        $timeout(function () {
+          $filter.affix({
+            offset: {
+              top: $filter.offset().top - ($toolbarDrawer.height() + $tabs.height())
+            }
+          }).on('affixed.bs.affix', function () {
+            $filter.css('top', $toolbarDrawer.height() + $tabs.height());
+            $feedBodyPanel.css('padding-top', $filter.outerHeight() + 15);
+          }).on('affixed-top.bs.affix', function () {
+            $filter.css('top', 'auto');
+            $feedBodyPanel.css('padding-top', '15px');
+          });
+        });
+      }
+
+      /**
+       * Resets Activity Filters affix offsets
+       */
+      function resetAffix () {
+        $timeout(function () {
+          // Reset right case view tab header
+          if ($filter.data('bs.affix')) {
+            $filter.data('bs.affix').options.offset.top = $filter.offset().top - ($toolbarDrawer.height() + $tabs.height());
+          }
+        });
+      }
+    }
+  });
+})(angular, CRM.$, CRM._);

ang/civicase/contact/contact-card.directive.js
@@ -1,0 +1,107 @@
+(function (angular, $, _, CRM) {
+  var module = angular.module('civicase');
+
+  module.directive('contactCard', function ($document, ContactsDataService) {
+    contactCardController.$inject = ['$scope'];
+
+    return {
+      restrict: 'A',
+      replace: true,
+      controller: contactCardController,
+      templateUrl: '~/civicase/contact/contact-card.directive.html',
+      scope: {
+        data: '=contacts',
+        isAvatar: '=avatar',
+        noIcon: '=noIcon'
+      }
+    };
+
+    function contactCardController (scope) {
+      scope.ts = CRM.ts('civicase');
+      scope.url = CRM.url;
+      scope.mainContact = null;
+
+      (function init () {
+        scope.$watch('data', refresh);
+      }());
+
+      /**
+       * Watch function for data refresh
+       */
+      function refresh () {
+        scope.contacts = [];
+
+        if (_.isPlainObject(scope.data)) {
+          _.each(scope.data, function (name, contactID) {
+            if (scope.isAvatar) {
+              prepareAvatarData(name, contactID);
+            } else {
+              scope.contacts.push({display_name: name, contact_id: contactID});
+            }
+          });
+        } else if (typeof scope.data === 'string') {
+          if (scope.isAvatar) {
+            prepareAvatarData(ContactsDataService.getCachedContact(scope.data).display_name, scope.data);
+          } else {
+            scope.contacts.push({display_name: ContactsDataService.getCachedContact(scope.data).display_name, contact_id: scope.data});
+          }
+        } else {
+          scope.contacts = _.cloneDeep(scope.data);
+        }
+      }
+
+      /**
+       * Get initials from the sent parameter
+       * Example: JD should be returned for John Doe
+       *
+       * @param {String} string
+       * @return {String}
+       */
+      function getInitials (string) {
+        var names = string.split(' ');
+        var initials = names[0].substring(0, 1).toUpperCase();
+
+        if (names.length > 1) {
+          initials += names[names.length - 1].substring(0, 1).toUpperCase();
+        }
+
+        return initials;
+      }
+
+      /**
+       * Prepares data when the directive is avatar
+       *
+       * @param {String} name
+       * @param {String} contactID
+       */
+      function prepareAvatarData (name, contactID) {
+        var avatarText;
+
+        if (validateEmail(name)) {
+          avatarText = name.substr(0, 1).toUpperCase();
+        } else {
+          avatarText = getInitials(name);
+        }
+
+        scope.contacts.push({
+          display_name: name,
+          contact_id: contactID,
+          avatar: avatarText,
+          image_URL: ContactsDataService.getImageUrlOf(contactID)
+        });
+      }
+
+      /**
+       * Checks whether the sent parameter is a valid email address
+       *
+       * @param {String} email
+       * @return {Boolean}
+       */
+      function validateEmail (email) {
+        var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()\\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+        return re.test(String(email).toLowerCase());
+      }
+    }
+  });
+})(angular, CRM.$, CRM._, CRM);

civicrm/org.civicrm.civicase

ang/civicase/activity/activity-feed/activity-filters-contact.directive.js
@@ -1,0 +1,73 @@
+(function (angular, $, _) {
+  var module = angular.module('civicase');
+
+  module.directive('civicaseActivityFiltersContact', function () {
+    return {
+      restrict: 'AE',
+      replace: true,
+      templateUrl: '~/civicase/activity/activity-feed/activity-filters-contact.directive.html',
+      scope: {
+        filters: '=civicaseActivityFiltersContact'
+      },
+      link: civicaseActivityFiltersContactLink
+    };
+
+    /**
+     * Link function for civicaseActivityFiltersContact
+     *
+     * @param {Object} $scope
+     */
+    function civicaseActivityFiltersContactLink ($scope) {
+      $scope.ts = CRM.ts('civicase');
+
+      (function init () {
+        $scope.$watch('filters', filtersWatcher);
+        $scope.$on('civicaseActivityFeed.query', feedQueryListener);
+      }());
+
+      /**
+       * Watch listener for filters
+       */
+      function filtersWatcher () {
+        // Ensure "All" checkbox renders.
+        if ($scope.filters['@involvingContact'] === undefined) {
+          $scope.filters['@involvingContact'] = '';
+        }
+      }
+
+      /**
+       * Subscribe listener for civicaseActivityFeed.query
+       *
+       * @param {Object} event
+       * @param {Object} filters
+       * @param {Object} params
+       * @param {Boolean} reset if it's necessary to delete the previously-set properties
+       */
+      function feedQueryListener (event, filters, params, reset) {
+        if (reset) {
+          delete params.contact_id;
+          delete params.assignee_contact_id;
+          delete params.source_contact_id;
+        }
+
+        switch (filters['@involvingContact']) {
+          case 'myActivities':
+            params.contact_id = 'user_contact_id';
+            break;
+
+          case 'delegated':
+            if (_.isEmpty(params.assignee_contact_id)) {
+              params.assignee_contact_id = {'!=': 'user_contact_id'};
+            }
+            if (_.isEmpty(params.source_contact_id)) {
+              params.source_contact_id = 'user_contact_id';
+            }
+            break;
+
+          default:
+            break;
+        }
+      }
+    }
+  });
+})(angular, CRM.$, CRM._);

ang/civicase/activity/activity-feed/activity-filters.directive.js
@@ -1,0 +1,265 @@
+(function (angular, $, _) {
+  var module = angular.module('civicase');
+
+  module.directive('civicaseActivityFilters', function ($timeout, crmUiHelp) {
+    return {
+      restrict: 'A',
+      scope: {
+        params: '=feedParams',
+        showCheckboxes: '=?',
+        bulkAllowed: '=',
+        caseTimelines: '=',
+        displayedCount: '=',
+        totalCount: '=',
+        filters: '=civicaseActivityFilters',
+        displayOptions: '=displayOptions',
+        selectedActivities: '='
+      },
+      replace: true,
+      templateUrl: '~/civicase/activity/activity-feed/activity-filters.directive.html',
+      link: activityFiltersLink,
+      transclude: true
+    };
+
+    /**
+     * Link function for civicaseActivityFilters
+     *
+     * @param {Object} $scope
+     * @param {Object} element
+     */
+    function activityFiltersLink ($scope, element) {
+      var ts = $scope.ts = CRM.ts('civicase');
+
+      $scope.activityCategories = prepareActivityCategories();
+      $scope.availableFilters = prepareAvailableFilters();
+      // Default exposed filters
+      $scope.exposedFilters = {
+        activity_type_id: true,
+        status_id: true,
+        assignee_contact_id: true,
+        tag_id: true,
+        text: true
+      };
+
+      (function init () {
+        // Ensure set filters are also exposed
+        _.each($scope.filters, function (filter, key) {
+          $scope.exposedFilters[key] = true;
+        });
+      }());
+
+      /**
+       * Exposes the selected filter in the UI
+       *
+       * @param {Object} field
+       * @param {Object} $event
+       */
+      $scope.exposeFilter = function (field, $event) {
+        var shown = !$scope.exposedFilters[field.name];
+        if (shown) {
+          // Focus search element when selecting
+          $timeout(function () {
+            var $span = $('[data-activity-filter=' + field.name + ']', element);
+
+            if ($('[crm-entityref], [crm-ui-select]', $span).length) {
+              $('[crm-entityref], [crm-ui-select]', $span).select2('open');
+            } else {
+              $('input:first', $span).focus();
+            }
+          }, 50);
+        } else {
+          // Keep menu open when deselecting
+          $event.stopPropagation();
+          delete $scope.filters[field.name];
+        }
+      };
+
+      /**
+       * Checks if any filter has been applied
+       *
+       * @return {Boolean}
+       */
+      $scope.hasFilters = function () {
+        var result = false;
+
+        _.each($scope.filters, function (value) {
+          if (!_.isEmpty(value)) result = true;
+        });
+
+        return result;
+      };
+
+      /**
+       * Clears all the filters
+       */
+      $scope.clearFilters = function () {
+        _.each(_.keys($scope.filters), function (key) {
+          delete $scope.filters[key];
+        });
+      };
+
+      /**
+       * Prepare Activity Filters
+       *
+       * @return {Array}
+       */
+      function prepareAvailableFilters () {
+        var availableFilters = [
+          {
+            name: 'activity_type_id',
+            label: ts('Activity type'),
+            html_type: 'Select',
+            options: _.map(CRM.civicase.activityTypes, mapSelectOptions)
+          },
+          {
+            name: 'status_id',
+            label: ts('Status'),
+            html_type: 'Select',
+            options: _.map(CRM.civicase.activityStatuses, mapSelectOptions)
+          },
+          {
+            name: 'target_contact_id',
+            label: ts('With'),
+            html_type: 'Autocomplete-Select',
+            entity: 'Contact'
+          },
+          {
+            name: 'assignee_contact_id',
+            label: ts('Assigned to'),
+            html_type: 'Autocomplete-Select',
+            entity: 'Contact'
+          },
+          {
+            name: 'tag_id',
+            label: ts('Tagged'),
+            html_type: 'Autocomplete-Select',
+            entity: 'Tag',
+            api_params: {used_for: {LIKE: '%civicrm_activity%'}}
+          },
+          {
+            name: 'text',
+            label: ts('Contains text'),
+            html_type: 'Text'
+          },
+          {
+            name: 'activity_date_time',
+            label: ts('Activity date'),
+            html_type: 'Select Date'
+          }
+        ];
+
+        if (_.includes(CRM.config.enableComponents, 'CiviCampaign')) {
+          availableFilters.push({
+            name: 'campaign_id', label: ts('Campaign'), html_type: 'Autocomplete-Select', entity: 'Campaign'
+          });
+        }
+        if (CRM.checkPerm('administer CiviCRM')) {
+          availableFilters.push({
+            name: 'is_deleted',
+            label: ts('Deleted Activities'),
+            html_type: 'Select',
+            options: [{id: 1, text: ts('Deleted')}, {id: 0, text: ts('Normal')}]
+          },
+          {
+            name: 'is_test',
+            label: ts('Test Activities'),
+            html_type: 'Select',
+            options: [{id: 1, text: ts('Test')}, {id: 0, text: ts('Normal')}]
+          });
+        }
+
+        availableFilters = availableFilters.concat(CRM.civicase.customActivityFields);
+
+        return availableFilters;
+      }
+
+      /**
+       * Prepare Activity Categories
+       *
+       * @return {Array}
+       */
+      function prepareActivityCategories () {
+        return _.map(CRM.civicase.activityCategories, function (category, key) {
+          category.id = key;
+          category.text = category.label;
+
+          return category;
+        });
+      }
+
+      /**
+       * Maps Options to be used in the dropdown
+       *
+       * @param {Object} option
+       * @param {int/string} id
+       * @return {Object}
+       */
+      function mapSelectOptions (option, id) {
+        return {
+          id: id,
+          text: option.label,
+          color: option.color,
+          icon: option.icon
+        };
+      }
+    }
+  });
+
+  module.directive('civicaseActivityFiltersAffix', function ($rootScope, $timeout) {
+    return {
+      link: civicaseActivityFiltersAffix
+    };
+
+    /**
+     * Link function for civicaseActivityFiltersAffix
+     *
+     * @param {Object} scope
+     * @param {Object} $el
+     * @param {Object} attr
+     */
+    function civicaseActivityFiltersAffix (scope, $el, attr) {
+      var $filter, $feedBodyPanel, $tabs, $toolbarDrawer;
+
+      (function init () {
+        affixActivityFilters();
+        $rootScope.$on('civicase::case-search::dropdown-toggle', resetAffix);
+      }());
+
+      /**
+       * Sets Activity Filters affix offsets
+       */
+      function affixActivityFilters () {
+        $filter = $('.civicase__activity-filter');
+        $feedBodyPanel = $('.civicase__activity-filter ~ .panel-body');
+        $tabs = $('.civicase__dashboard').length > 0 ? $('.civicase__dashboard__tab-container ul.nav') : $('.civicase__case-body_tab');
+        $toolbarDrawer = $('#toolbar');
+
+        $timeout(function () {
+          $filter.affix({
+            offset: {
+              top: $filter.offset().top - ($toolbarDrawer.height() + $tabs.height())
+            }
+          }).on('affixed.bs.affix', function () {
+            $filter.css('top', $toolbarDrawer.height() + $tabs.height());
+            $feedBodyPanel.css('padding-top', $filter.outerHeight() + 15);
+          }).on('affixed-top.bs.affix', function () {
+            $filter.css('top', 'auto');
+            $feedBodyPanel.css('padding-top', '15px');
+          });
+        });
+      }
+
+      /**
+       * Resets Activity Filters affix offsets
+       */
+      function resetAffix () {
+        $timeout(function () {
+          // Reset right case view tab header
+          if ($filter.data('bs.affix')) {
+            $filter.data('bs.affix').options.offset.top = $filter.offset().top - ($toolbarDrawer.height() + $tabs.height());
+          }
+        });
+      }
+    }
+  });
+})(angular, CRM.$, CRM._);

ang/civicase/contact/contact-card.directive.js
@@ -1,0 +1,107 @@
+(function (angular, $, _, CRM) {
+  var module = angular.module('civicase');
+
+  module.directive('contactCard', function ($document, ContactsCache) {
+    contactCardController.$inject = ['$scope'];
+
+    return {
+      restrict: 'A',
+      replace: true,
+      controller: contactCardController,
+      templateUrl: '~/civicase/contact/contact-card.directive.html',
+      scope: {
+        data: '=contacts',
+        isAvatar: '=avatar',
+        noIcon: '=noIcon'
+      }
+    };
+
+    function contactCardController (scope) {
+      scope.ts = CRM.ts('civicase');
+      scope.url = CRM.url;
+      scope.mainContact = null;
+
+      (function init () {
+        scope.$watch('data', refresh);
+      }());
+
+      /**
+       * Watch function for data refresh
+       */
+      function refresh () {
+        scope.contacts = [];
+
+        if (_.isPlainObject(scope.data)) {
+          _.each(scope.data, function (name, contactID) {
+            if (scope.isAvatar) {
+              prepareAvatarData(name, contactID);
+            } else {
+              scope.contacts.push({display_name: name, contact_id: contactID});
+            }
+          });
+        } else if (typeof scope.data === 'string') {
+          if (scope.isAvatar) {
+            prepareAvatarData(ContactsCache.getCachedContact(scope.data).display_name, scope.data);
+          } else {
+            scope.contacts.push({display_name: ContactsCache.getCachedContact(scope.data).display_name, contact_id: scope.data});
+          }
+        } else {
+          scope.contacts = _.cloneDeep(scope.data);
+        }
+      }
+
+      /**
+       * Get initials from the sent parameter
+       * Example: JD should be returned for John Doe
+       *
+       * @param {String} string
+       * @return {String}
+       */
+      function getInitials (string) {
+        var names = string.split(' ');
+        var initials = names[0].substring(0, 1).toUpperCase();
+
+        if (names.length > 1) {
+          initials += names[names.length - 1].substring(0, 1).toUpperCase();
+        }
+
+        return initials;
+      }
+
+      /**
+       * Prepares data when the directive is avatar
+       *
+       * @param {String} name
+       * @param {String} contactID
+       */
+      function prepareAvatarData (name, contactID) {
+        var avatarText;
+
+        if (validateEmail(name)) {
+          avatarText = name.substr(0, 1).toUpperCase();
+        } else {
+          avatarText = getInitials(name);
+        }
+
+        scope.contacts.push({
+          display_name: name,
+          contact_id: contactID,
+          avatar: avatarText,
+          image_URL: ContactsCache.getImageUrlOf(contactID)
+        });
+      }
+
+      /**
+       * Checks whether the sent parameter is a valid email address
+       *
+       * @param {String} email
+       * @return {Boolean}
+       */
+      function validateEmail (email) {
+        var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()\\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+        return re.test(String(email).toLowerCase());
+      }
+    }
+  });
+})(angular, CRM.$, CRM._, CRM);