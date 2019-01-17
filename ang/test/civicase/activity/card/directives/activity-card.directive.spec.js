5c872e4c85fcb7e45b4d4d502bd363a72775b4cf
Debarshi Bhaumik <deb1990@gmail.com>
2019-01-17 14:40:10 +0530

    C51-222: unit test

--- ang/test/civicase/activity/card/directives/activity-card.directive.spec.js
+++ ang/test/civicase/activity/card/directives/activity-card.directive.spec.js
@@ -2,13 +2,14 @@
 
 (function ($) {
   describe('ActivityCard', function () {
-    var $compile, $rootScope, $scope, activityCard;
+    var $compile, $rootScope, $scope, activityCard, activitiesMockData;
 
-    beforeEach(module('civicase', 'civicase.templates'));
+    beforeEach(module('civicase', 'civicase.templates', 'civicase.data'));
 
-    beforeEach(inject(function (_$compile_, _$rootScope_) {
+    beforeEach(inject(function (_$compile_, _$rootScope_, _activitiesMockData_) {
       $compile = _$compile_;
       $rootScope = _$rootScope_;
+      activitiesMockData = _activitiesMockData_;
 
       $('<div id="bootstrap-theme"></div>').appendTo('body');
       initDirective();
@@ -24,14 +25,66 @@
       });
     });
 
+    describe('when editing an activity in the popup', function () {
+      var activity, caseObj, loadFormSpy, crmUrlReturnVal, crmFormSuccessCallback;
+
+      beforeEach(function () {
+        crmUrlReturnVal = 'some string';
+        activity = activitiesMockData.get()[0];
+        caseObj = {
+          client: [{
+            contact_id: '1'
+          }]
+        };
+
+        activityCard.isolateScope().case = caseObj;
+
+        loadFormSpy = jasmine.createSpyObj('loadForm', ['on']);
+        loadFormSpy.on.and.callFake(function (event, fn) {
+          crmFormSuccessCallback = fn;
+        });
+
+        CRM.loadForm.and.returnValue(loadFormSpy);
+        CRM.url.and.returnValue(crmUrlReturnVal);
+
+        activityCard.isolateScope().viewInPopup(null, activity);
+      });
+
+      it('opens the modal to edit the activity', function () {
+        expect(CRM.url).toHaveBeenCalledWith('civicrm/case/activity', {
+          action: 'update',
+          id: activity.id,
+          cid: caseObj.client[0].contact_id,
+          caseid: activity.case_id,
+          reset: 1
+        });
+        expect(CRM.loadForm).toHaveBeenCalledWith(crmUrlReturnVal);
+      });
+
+      it('listenes for the the form to be saved', function () {
+        expect(loadFormSpy.on).toHaveBeenCalledWith('crmFormSuccess', jasmine.any(Function));
+      });
+
+      describe('when activity is saved', function () {
+        beforeEach(function () {
+          crmFormSuccessCallback();
+        });
+
+        it('refreshes the data when activity is saved', function () {
+          expect(activityCard.isolateScope().refresh).toHaveBeenCalled();
+        });
+      });
+    });
+
     /**
      * Initializes the ActivityCard directive
      */
     function initDirective () {
       $scope = $rootScope.$new();
       $scope.activity = {};
+      $scope.refreshCallback = jasmine.createSpy('refreshCallback');
 
-      activityCard = $compile('<div case-activity-card="activity"></div>')($scope);
+      activityCard = $compile('<div case-activity-card="activity" refresh-callback="refreshCallback"></div>')($scope);
       $rootScope.$digest();
     }
   });
