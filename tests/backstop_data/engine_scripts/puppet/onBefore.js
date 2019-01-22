63470cc6eecfa1281a95b94ed01656e031ee3a18
Swastik Pareek <swastik1608@gmail.com>
2019-01-22 14:03:06 +0530

    C51-386: PR feedback - Fix typo and remove unnecessary files

--- tests/backstop_data/engine_scripts/puppet/onBefore.js
+++ tests/backstop_data/engine_scripts/puppet/onBefore.js
@@ -4,6 +4,5 @@ module.exports = async (page, scenario, vp) => {
   console.log('--------------------------------------------');
   console.log('Running Scenario "' + scenario.label + '" ' + scenario.count);
 
-  await require('./clickAndHoverHelper')(page, scenario);
   await require('./loadCookies')(page, scenario);
 };
