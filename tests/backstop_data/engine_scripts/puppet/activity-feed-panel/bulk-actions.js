c6e4106f87d60ddbf5706d6df18af8b9e0f93346
René Olivo <minet.ws@gmail.com>
2019-02-26 11:32:01 +0000

    C51-392: Add comment about animation delay

--- tests/backstop_data/engine_scripts/puppet/activity-feed-panel/bulk-actions.js
+++ tests/backstop_data/engine_scripts/puppet/activity-feed-panel/bulk-actions.js
@@ -8,6 +8,7 @@ module.exports = async (page, scenario, vp) => {
   await utility.waitForAngular();
   await utility.waitForLoadingComplete();
   await page.click('.civicase__bulkactions-checkbox-toggle');
+  // this waits for the animation to finish before continue:
   await page.waitFor(300);
   await page.click('.civicase__checkbox--bulk-action');
 };
