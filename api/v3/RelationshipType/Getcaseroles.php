89300f0d3c30ede6aed77ca302d46cea8a73e8ed
René Olivo <minet.ws@gmail.com>
2019-03-13 14:35:44 +0000

    C51-411: PR Feedback

--- api/v3/RelationshipType/Getcaseroles.php
+++ api/v3/RelationshipType/Getcaseroles.php
@@ -1,26 +1,37 @@
 <?php
 
 function _civicrm_api3_relationship_type_getcaseroles_spec(&$spec) {
-  // no specs
+  $spec = CRM_Contact_DAO_RelationshipType::fields();
 }
 
 function civicrm_api3_relationship_type_getcaseroles($params) {
-  $rolesMap = [];
+  $caseRoleNames = _civicrm_api3_relationship_type_getcaseroles_getCaseRoleNames();
+
+  $params['label_b_a'] = [ 'IN' => $caseRoleNames ];
+  $params['is_active'] = 1;
+
+  return civicrm_api3('RelationshipType', 'get', $params);
+}
+
+/**
+ * Returns a list of relationship type names that have been associated to
+ * case types
+ *
+ * @return array
+ */
+function _civicrm_api3_relationship_type_getcaseroles_getCaseRoleNames () {
+  $rolenamesMap = [];
   $caseTypes = civicrm_api3('CaseType', 'get', [
     'sequential' => 1,
     'is_active' => 1,
+    'options' => [ 'limit' => 0 ],
   ]);
 
   foreach ($caseTypes['values'] as $caseType) {
     foreach ($caseType['definition']['caseRoles'] as $role) {
-      $rolesMap[$role['name']] = true;
+      $rolenamesMap[$role['name']] = true;
     }
   }
 
-  $relationshipTypes = civicrm_api3('RelationshipType', 'get', [
-    'label_b_a' => ['IN' => array_keys($rolesMap)],
-    'is_active' => 1,
-  ]);
-
-  return $relationshipTypes;
+  return array_keys($rolenamesMap);
 }
