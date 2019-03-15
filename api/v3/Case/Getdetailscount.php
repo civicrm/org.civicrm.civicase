ae3073298647d692ca402ccd59c4b11e1d309910
Debarshi Bhaumik <deb1990@gmail.com>
2019-03-15 13:50:22 +0530

    C51-401: Fix pager counts

--- api/v3/Case/Getdetailscount.php
+++ api/v3/Case/Getdetailscount.php
@@ -11,12 +11,13 @@
  */
 function civicrm_api3_case_getdetailscount($params) {
   $params['options'] = CRM_Utils_Array::value('options', $params, []);
-  $params['options']['is_count'] = 1;
+  // There is issue in the core with using $params['options']['is_count'] = 1;
+  // It does not remove the duplicate results.
 
   // Remove unnecesary parameters:
   unset($params['return'], $params['sequential']);
 
   $casesList = civicrm_api3('Case', 'getdetails', $params);
 
-  return $casesList['values'];
+  return $casesList['count'];
 }
